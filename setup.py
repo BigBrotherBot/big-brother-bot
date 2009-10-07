cbd96f03e9e831838ce65e33dff04e94d14cf446
xlr8or <xlr8or@xlr8or.com>
2009-10-07 20:45:37 +0200

    Prepared for version 1.2.1b with remote support

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ if len(sys.argv) == 1:
 
 setup(
     name = "b3",
-    version = "1.2.0",
+    version = "1.2.1",
     packages = find_packages(),
     extras_require = { 'mysql' : 'MySQL-python', 'elementtree' : 'elementtree' },
     package_data = {
