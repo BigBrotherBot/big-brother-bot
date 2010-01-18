6688e5e7126c3494b52735e410ace5109ed04e59
Courgette <courgette@ubu-team.org>
2010-01-18 02:10:10 +0100

    listversion script improved to also find differences between individual files

--- listversions.py
+++ listversions.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-For all *.py files within the current directory
+For all *.py files within the b3 directory
 search for __version__
 compute a md5 sum from file names and versions to 
 make it easy to compare two branches/repositories
@@ -12,7 +12,7 @@ import hashlib
 
 
 fileList = []
-rootdir = '.'
+rootdir = 'b3'
 for root, subFolders, files in os.walk(rootdir):
     for file in files:
         if file.endswith('.py'):
@@ -22,25 +22,35 @@ fileList.sort()
 reVersion = re.compile(r'^\s*__version__\s*=\s*[\'"](?P<version>.+)[\'"].*$', re.MULTILINE)
 
 
-resultsLines = []
+filesAndVersions = []
+linesToDisplay = []
+md5sum_content = hashlib.md5()
 for pythonfile in fileList:
     try:
-        str = pythonfile + ' '
+        str = pythonfile
         f = open(pythonfile)
         filecontent = f.read()
         
+        md5sum_content.update(filecontent)
+        
+        md5sum_tmp = hashlib.md5()
+        md5sum_tmp.update(filecontent)
+        
+        version = None
         m = reVersion.search(filecontent)
         if m:
-            str += m.group('version')
-        resultsLines.append(str)
+            version = m.group('version')
+        filesAndVersions.append('%s %s' % (pythonfile, version))
+        linesToDisplay.append('%s %s %s' % (pythonfile, md5sum_tmp.hexdigest(), version))
     finally:
         f.close()
-        
-md5sum = hashlib.md5()
-for line in resultsLines:
-    md5sum.update(line.replace('\\','/'))
-print "project signature: %s" % md5sum.hexdigest()
+
+md5sum_fnames_versions = hashlib.md5()
+for line in filesAndVersions:
+    md5sum_fnames_versions.update(line.replace('\\','/'))
+print "project files/versions signature: %s" % md5sum_fnames_versions.hexdigest()
+print "project files content signature: %s" % md5sum_content.hexdigest()
  
-for line in resultsLines:
+for line in linesToDisplay:
     print line
         
