001e706b9e58ea2cfb9d6dc351da8314c6f7fa11
xlr8or <xlr8or@xlr8or.com>
2010-10-22 20:16:53 +0200

    versioning

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ from setuptools import setup, find_packages
 
 _setupinfo = {
     'name' : "b3",
-    'version' : "1.4.0",
+    'version' : "1.4.1b",
     'packages' : find_packages(),
     'extras_require' : { 'mysql' : 'MySQL-python' },
     'package_data' : {
