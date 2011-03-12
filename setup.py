20706e4b2c85c9e2514e3de0381b4fc3b2df5362
xlr8or <xlr8or@xlr8or.com>
2011-03-12 21:02:08 +0100

    Versioning to 1.5.0b

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ from setuptools import setup, find_packages
 
 _setupinfo = {
     'name' : "b3",
-    'version' : "1.4.2b",
+    'version' : "1.5.0b",
     'packages' : find_packages(),
     'extras_require' : { 'mysql' : 'MySQL-python' },
     'package_data' : {
