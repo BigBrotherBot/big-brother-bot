a38530e280964a499e834d5f6c26c9ac58a8674f
82ndab-Bravo17 <82ndab.bravo17@gmail.com>
2011-03-29 21:36:08 -0400

    Added getMap method and reworked getNextMap to use it.

--- b3/parsers/homefront/__init__.py
+++ b3/parsers/homefront/__init__.py
@@ -499,7 +499,10 @@ class HomefrontParser(b3.parser.Parser):
         """\
         return the current map/level name
         """
-        raise NotImplementedError
+        if self._currentmap is None:
+            return "Unknown"
+        else:
+            return self._currentmap
 
     def getNextMap(self):
         """Return the name of the next map
@@ -507,8 +510,9 @@ class HomefrontParser(b3.parser.Parser):
         nextmap=''
         self.getMaps()
         no_maps = len(self.maplist)
-        if self.maplist.count(self._currentmap) == 1:
-            i = self.maplist.index(self._currentmap)
+        currentmap = self.getMap()
+        if self.maplist.count(currentmap) == 1:
+            i = self.maplist.index(currentmap)
             if i < no_maps-1:
                 nextmap = self.mapgamelist[i+1]
             else:
