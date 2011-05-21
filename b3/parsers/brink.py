620d5423d37cfc08147acbea3a6a418eb3201b62
Courgette <courgette@bigbrotherbot.net>
2011-05-21 12:43:49 +0200

    BRINK: more progress, still no way to get in-game chat

--- b3/parsers/brink.py
+++ b3/parsers/brink.py
@@ -18,13 +18,13 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 #
-from b3.events import EVT_UNKNOWN
+from b3.cvar import Cvar
+from b3.lib.sourcelib import SourceQuery
 from b3.parser import Parser
 from b3.parsers.q3a.rcon import Rcon as Q3Rcon
-import b3.events
+import b3
 import re
 import select
-import string
 __author__  = 'Courgette'
 __version__ = '0.0'
 
@@ -79,21 +79,42 @@ class BrinkParser(Parser):
     gameName = 'brink'
     OutputClass = BrinkRcon
 
+    _reCvarName = re.compile(r'^[a-z0-9_.]+$', re.I)
+    _reCvar = re.compile(r'^"(?P<cvar>[a-z0-9_.]+)" is:"(?P<value>.*?)"\^7 default:"(?P<default>.*?)".*', re.I)
+
     # remove the time off of the line
     _lineTime  = re.compile(r'^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:(?P<minutes>[0-9]+):(?P<seconds>[0-9]+).*')
     _lineClear = re.compile(r'^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]+:[0-9]+ : ')
 
     _lineFormats = (
         # OnClientConnect: Clearing abilities for client 15
-        ("OnClientConnect", re.compile(r'^OnClientConnect: Clearing abilities for client (?P<cid>[0-9]+)$')),
+        ("OnClientConnect", re.compile(r'^client (?P<cid>[0-9]+) connected.$')),
         ("OnClientDisconnect", re.compile(r'^client (?P<cid>[0-9]+) disconnected.$')),
+        ("OnClientDisconnectByName", re.compile(r'^(?P<name>.+) disconnected.$')),
+        ("OnClientTeamChange", re.compile(r'^(?P<name>.+) joined the (?P<team>.+)$')),
+        ("OnInitFromNewMap", re.compile(r"^InitFromNewMap: 'maps/mp/(?P<map>.+).entities'$")),
+        ("OnClientClassChange", re.compile(r"^(?P<cid>[0-9]+) given player class '(?P<class>.+)' \(clients desired player class\)$")),
     )
+    
+    queryPort = None
+    sourceQueryClient = None
 
     def startup(self):
-        pass
+        self.setCvar('logTimeStamps', "1")
+        self.setCvar('logFileFilter', "WARNING:")
+        self.setCvar('logFile', "2")
+        
+        self.queryPort = self.getCvar("net_serverPortMaster").getInt()
+        self.info("query port is : %s", self.queryPort)
+        self.sourceQueryClient = SourceQuery.SourceQuery(self._rconIp, self.queryPort, timeout=5)
+        
+        self.queryGameInfo()
+        self.info("connected players : %r" % self.getPlayerList())
+
 
     def getLineParts(self, line):
         line = re.sub(self._lineClear, '', line, 1)
+        f = None
         for f in self._lineFormats:
             m = re.match(f[1], line)
             if m:
@@ -111,12 +132,28 @@ class BrinkParser(Parser):
             func = getattr(self, action)
             event = func(line, match)
             if event:
+                self.verbose2("queuing event %r ", event)
                 self.queueEvent(event)
         else:
             self.verbose2("Implement method : def %s(self, line, match):", action)
-            self.queueEvent(b3.events.Event(EVT_UNKNOWN, "%s: %s" % (action, line)))
+            self.queueEvent(self.getEvent('EVT_UNKNOWN', data="%s: %s" % (action, line)))
 
+    def getCvar(self, cvarName):
+        if self._reCvarName.match(cvarName):
+            #"g_password" is:"^7" default:"scrim^7"
+            val = self.write(cvarName)
+            self.debug('Get cvar %s = [%s]', cvarName, val)
+            #sv_mapRotation is:gametype sd map mp_brecourt map mp_carentan map mp_dawnville map mp_depot map mp_harbor map mp_hurtgen map mp_neuville map mp_pavlov map mp_powcamp map mp_railyard map mp_rocket map mp_stalingrad^7 default:^7
+            m = self._reCvar.match(val)
+            if m and m.group('cvar').lower() == cvarName.lower():
+                return Cvar(m.group('cvar'), value=m.group('value'), default=m.group('default'))
 
+    def setCvar(self, cvarName, value):
+        if re.match('^[a-z0-9_.]+$', cvarName, re.I):
+            self.debug('Set cvar %s = [%s]', cvarName, value)
+            self.write('set %s %s' % (cvarName, value))
+        else:
+            self.error('%s is not a valid cvar name', cvarName)
 
 
     # ================================================
@@ -127,17 +164,70 @@ class BrinkParser(Parser):
     # ================================================
     
     def OnClientConnect(self, line, match):
+        """
+        example: client 4 connected.
+        """
         cid = match.group('cid')
         if cid:
-            self.clients.newClient(cid)
+            # find out its name
+            players = self.getPlayerList()
+            if cid in players:
+                data = players[cid]
+                client = self.clients.newClient(cid, name=data['name'], data=data)
+                self.debug("created client %r " % client)
             
     def OnClientDisconnect(self, line, match):
+        """
+        example: client 4 Disconnected.
+        """
         cid = match.group('cid')
         if cid:
             client = self.clients.getByCID(cid)
             if client:
                 client.disconnect()
+            
+    def OnClientDisconnectByName(self, line, match):
+        """
+        example: medvidek Disconnected.
+        """
+        name = match.group('name')
+        if name:
+            client = self.clients.getByExactName(name)
+            if client:
+                client.disconnect()
         
+    def OnClientTeamChange(self, line, match):
+        name = match.group('name')
+        team = match.group('team')
+        if name and team:
+            client = self.clients.getByExactName(name)
+            if client:
+                client.team = self.getTeam(team)
+            return self.getEvent('EVT_CLIENT_JOIN', data=team, client=client)
+        
+    def OnInitFromNewMap(self, line, match):
+        """
+        example: InitFromNewMap: 'maps/mp/reactor.entities'
+        """
+        map = match.group('map')
+        if map:
+            self.game.mapName = map
+            self.game.startMap()
+        
+    def OnClientClassChange(self, line, match):
+        """
+        example: 10 given player class 'soldier' (clients desired player class)
+        """
+        cid = match.group('cid')
+        playerclass = match.group('class')
+        if cid and playerclass:
+            client = self.clients.getByCID(cid)
+            if client:
+                client.data = {'class': playerclass}
+                self.debug("updated %s class to %s ", client, playerclass)
+
+
+
 
     # =======================================
     # implement parser interface
@@ -148,7 +238,14 @@ class BrinkParser(Parser):
         Query the game server for connected players.
         return a dict having players' id for keys and players' data as another dict for values
         """
-        raise NotImplementedError
+        data = self.sourceQueryClient.player()
+        if not data:
+            return {}
+        self.debug(data)
+        players = {}
+        for pdata in data:
+            players[pdata['index']] = pdata
+        return players
 
     def authorizeClients(self):
         """\
@@ -260,6 +357,47 @@ class BrinkParser(Parser):
         pass
 
 
+    #===============================================================================
+    # 
+    # convenience methods
+    # 
+    #===============================================================================
+
+    def getTeam(self, id):
+        """convert a Brink team identifier into a B3 team id"""
+        if id == 'Resistance':
+            return b3.TEAM_BLUE
+        elif id == 'Security':
+            return b3.TEAM_RED
+        elif id == 'Spectators':
+            return b3.TEAM_SPEC
+        else:
+            return b3.TEAM_UNKNOWN
+        
+    def queryGameInfo(self):
+        """use the Source Query Protocol to get game info
+        
+        {'steamid': 90083332288439317L, 'map': 'mp/aquarium', 'dedicated': 'd', 
+        'gamedir': 'Brink', 'secure': 1, 'numbots': 0, 'gamedesc': 'Brink', 
+        'hostname': 'Cucurb B3 test server', 'numplayers': 1, 'maxplayers': 16, 
+        'version': '0.0.0.0', 'appid': 22350, 'edf': 177, 'passworded': 0, 
+        'tag': 'apv=2621477,mr=sdGameRulesObjective,pm=2,q=social,dlc=0,ded=1,hij=0,pwd=0,t1=15,t2=0,pnr=1,pl=0,bd=0,sr=2', 
+        'os': 'w', 'port': 27025}
+        """
+        data = self.sourceQueryClient.info()
+        if data:
+            self.game.mapName = data.pop('map')
+            datatags = data.pop('tag')
+            tags = {}
+            for t in datatags.split(','):
+                if not t: continue
+                k,v = t.split('=')
+                tags[k] = v
+            self.game.gameType = tags['mr']
+            for k,v in data.iteritems():
+                self.game[k] = v
+        self.debug("game : %r" % self.game)
+        
 
 if __name__ == '__main__':
     from b3.fake import fakeConsole
