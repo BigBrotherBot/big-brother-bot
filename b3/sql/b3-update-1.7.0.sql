0560c7a57b1e5a168b4e768f60736cc97d175484
Courgette <courgette@bigbrotherbot.net>
2011-05-27 03:51:40 +0200

    fix sql update file for B3 1.7

--- b3/sql/b3-update-1.7.0.sql
+++ b3/sql/b3-update-1.7.0.sql
@@ -1,14 +1,14 @@
 -- SQL code to update default B3 database tables to B3 version 1.7.0 --
 
 -- add ipaliases table --
-CREATE TABLE IF NOT EXISTS ipaliases (
-  id int(10) unsigned NOT NULL auto_increment,
-  num_used int(10) unsigned NOT NULL default '0',
-  ip varchar(16) NOT NULL,
-  client_id int(10) unsigned NOT NULL default '0',
-  time_add int(10) unsigned NOT NULL default '0',
-  time_edit int(10) unsigned NOT NULL default '0',
-  PRIMARY KEY  (id),
-  UNIQUE KEY ipalias (ip,client_id),
-  KEY client_id (client_id)
+CREATE TABLE IF NOT EXISTS `ipaliases` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `num_used` int(10) unsigned NOT NULL DEFAULT '0',
+  `ip` varchar(32) NOT NULL DEFAULT '',
+  `client_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `time_add` int(10) unsigned NOT NULL DEFAULT '0',
+  `time_edit` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ip` (`ip`,`client_id`),
+  KEY `client_id` (`client_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
