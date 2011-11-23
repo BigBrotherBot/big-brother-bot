f4170560fc68a967b3da2c9ad6d87054ab55c246
Courgette <courgette@bigbrotherbot.net>
2011-11-23 22:42:47 +0100

    BF3: follow up on BF3 R9 changes

---
+++ test/parsers/test_bf3.py
@@ -0,0 +1,363 @@
+#
+# BigBrotherBot(B3) (www.bigbrotherbot.net)
+# Copyright (C) 2011 Courgette
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+import unittest
+from mock import Mock
+from parsers.bf3 import Bf3Parser
+
+class Test_Bf3Parser(unittest.TestCase):
+
+    def test_decodeServerinfo_pre_R9(self):
+        self.assertDictContainsSubset({
+            'serverName': 'BigBrotherBot #2',
+            'numPlayers': '0',
+            'maxPlayers': '16',
+            'gamemode': 'ConquestLarge0',
+            'level': 'MP_012',
+            'roundsPlayed': '0',
+            'roundsTotal': '2',
+            'numTeams': '0',
+            'team1score': None,
+            'team2score': None,
+            'team3score': None,
+            'team4score': None,
+            'targetScore': '0',
+            'onlineState': '',
+            'isRanked': 'true',
+            'hasPunkbuster': 'true',
+            'hasPassword': 'false',
+            'serverUptime': '5148',
+            'roundTime': '455',
+        }, Bf3Parser.decodeServerinfo(('BigBrotherBot #2', '0', '16', 'ConquestLarge0', 'MP_012', '0', '2', '0', '0', '', 'true', 'true', 'false', '5148', '455')))
+
+        self.assertDictContainsSubset({
+            'serverName': 'BigBrotherBot #2',
+            'numPlayers': '0',
+            'maxPlayers': '16',
+            'gamemode': 'ConquestLarge0',
+            'level': 'MP_012',
+            'roundsPlayed': '0',
+            'roundsTotal': '2',
+            'numTeams': '1',
+            'team1score': '47',
+            'team2score': None,
+            'team3score': None,
+            'team4score': None,
+            'targetScore': '0',
+            'onlineState': '',
+            'isRanked': 'true',
+            'hasPunkbuster': 'true',
+            'hasPassword': 'false',
+            'serverUptime': '5148',
+            'roundTime': '455',
+        }, Bf3Parser.decodeServerinfo(('BigBrotherBot #2', '0', '16', 'ConquestLarge0', 'MP_012', '0', '2', '1', '47', '0', '', 'true', 'true', 'false', '5148', '455')))
+
+        self.assertDictContainsSubset({
+            'serverName': 'BigBrotherBot #2',
+            'numPlayers': '0',
+            'maxPlayers': '16',
+            'gamemode': 'ConquestLarge0',
+            'level': 'MP_012',
+            'roundsPlayed': '0',
+            'roundsTotal': '2',
+            'numTeams': '2',
+            'team1score': '300',
+            'team2score': '300',
+            'team3score': None,
+            'team4score': None,
+            'targetScore': '0',
+            'onlineState': '',
+            'isRanked': 'true',
+            'hasPunkbuster': 'true',
+            'hasPassword': 'false',
+            'serverUptime': '5148',
+            'roundTime': '455',
+        }, Bf3Parser.decodeServerinfo(('BigBrotherBot #2', '0', '16', 'ConquestLarge0', 'MP_012', '0', '2', '2', '300', '300', '0', '', 'true', 'true', 'false', '5148', '455')))
+
+        self.assertDictContainsSubset({
+            'serverName': 'BigBrotherBot #2',
+            'numPlayers': '0',
+            'maxPlayers': '16',
+            'gamemode': 'ConquestLarge0',
+            'level': 'MP_012',
+            'roundsPlayed': '1',
+            'roundsTotal': '2',
+            'numTeams': '3',
+            'team1score': '300',
+            'team2score': '215',
+            'team3score': '25',
+            'team4score': None,
+            'targetScore': '0',
+            'onlineState': '',
+            'isRanked': 'true',
+            'hasPunkbuster': 'true',
+            'hasPassword': 'false',
+            'serverUptime': '5148',
+            'roundTime': '455',
+        }, Bf3Parser.decodeServerinfo(('BigBrotherBot #2', '0', '16', 'ConquestLarge0', 'MP_012', '1', '2', '3', '300', '215', '25', '0', '', 'true', 'true', 'false', '5148', '455')))
+
+        self.assertDictContainsSubset({
+            'serverName': 'BigBrotherBot #2',
+            'numPlayers': '0',
+            'maxPlayers': '16',
+            'gamemode': 'ConquestLarge0',
+            'level': 'MP_012',
+            'roundsPlayed': '1',
+            'roundsTotal': '2',
+            'numTeams': '4',
+            'team1score': '300',
+            'team2score': '215',
+            'team3score': '25',
+            'team4score': '84',
+            'targetScore': '0',
+            'onlineState': '',
+            'isRanked': 'true',
+            'hasPunkbuster': 'true',
+            'hasPassword': 'false',
+            'serverUptime': '5148',
+            'roundTime': '455',
+        }, Bf3Parser.decodeServerinfo(('BigBrotherBot #2', '0', '16', 'ConquestLarge0', 'MP_012', '1', '2', '4', '300', '215', '25', '84', '0', '', 'true', 'true', 'false', '5148', '455')))
+
+    def test_decodeServerinfo_R9(self):
+        self.maxDiff = None
+        self.assertDictEqual({
+            'serverName': 'BigBrotherBot #2',
+            'numPlayers': '0',
+            'maxPlayers': '16',
+            'gamemode': 'ConquestLarge0',
+            'level': 'MP_012',
+            'roundsPlayed': '0',
+            'roundsTotal': '2',
+            'numTeams': '0',
+            'team1score': None,
+            'team2score': None,
+            'team3score': None,
+            'team4score': None,
+            'targetScore': '0',
+            'onlineState': '',
+            'isRanked': 'true',
+            'hasPunkbuster': 'true',
+            'hasPassword': 'false',
+            'serverUptime': '5148',
+            'roundTime': '455',
+            'gameIpAndPort': '1.2.3.4:5445',
+            'punkBusterVersion': '1.5',
+            'joinQueueEnabled': 'false',
+            'region': 'EU',
+            'closestPingSite': '45',
+            'country': 'FR',
+        }, Bf3Parser.decodeServerinfo(('BigBrotherBot #2', '0', '16', 'ConquestLarge0', 'MP_012', '0', '2', '0', '0', '', 'true', 'true', 'false', '5148', '455', '1.2.3.4:5445', '1.5', 'false', 'EU', '45', 'FR')))
+
+        self.assertDictEqual({
+            'serverName': 'BigBrotherBot #2',
+            'numPlayers': '0',
+            'maxPlayers': '16',
+            'gamemode': 'ConquestLarge0',
+            'level': 'MP_012',
+            'roundsPlayed': '0',
+            'roundsTotal': '2',
+            'numTeams': '1',
+            'team1score': '47',
+            'team2score': None,
+            'team3score': None,
+            'team4score': None,
+            'targetScore': '0',
+            'onlineState': '',
+            'isRanked': 'true',
+            'hasPunkbuster': 'true',
+            'hasPassword': 'false',
+            'serverUptime': '5148',
+            'roundTime': '455',
+            'gameIpAndPort': '1.2.3.4:5445',
+            'punkBusterVersion': '1.5',
+            'joinQueueEnabled': 'false',
+            'region': 'EU',
+            'closestPingSite': '45',
+            'country': 'FR',
+        }, Bf3Parser.decodeServerinfo(('BigBrotherBot #2', '0', '16', 'ConquestLarge0', 'MP_012', '0', '2', '1', '47', '0', '', 'true', 'true', 'false', '5148', '455', '1.2.3.4:5445', '1.5', 'false', 'EU', '45', 'FR')))
+
+        self.assertDictEqual({
+            'serverName': 'BigBrotherBot #2',
+            'numPlayers': '0',
+            'maxPlayers': '16',
+            'gamemode': 'ConquestLarge0',
+            'level': 'MP_012',
+            'roundsPlayed': '0',
+            'roundsTotal': '2',
+            'numTeams': '2',
+            'team1score': '300',
+            'team2score': '300',
+            'team3score': None,
+            'team4score': None,
+            'targetScore': '0',
+            'onlineState': '',
+            'isRanked': 'true',
+            'hasPunkbuster': 'true',
+            'hasPassword': 'false',
+            'serverUptime': '5148',
+            'roundTime': '455',
+            'gameIpAndPort': '1.2.3.4:5445',
+            'punkBusterVersion': '1.5',
+            'joinQueueEnabled': 'false',
+            'region': 'EU',
+            'closestPingSite': '45',
+            'country': 'FR',
+        }, Bf3Parser.decodeServerinfo(('BigBrotherBot #2', '0', '16', 'ConquestLarge0', 'MP_012', '0', '2', '2', '300', '300', '0', '', 'true', 'true', 'false', '5148', '455', '1.2.3.4:5445', '1.5', 'false', 'EU', '45', 'FR')))
+
+        self.assertDictEqual({
+            'serverName': 'BigBrotherBot #2',
+            'numPlayers': '0',
+            'maxPlayers': '16',
+            'gamemode': 'ConquestLarge0',
+            'level': 'MP_012',
+            'roundsPlayed': '1',
+            'roundsTotal': '2',
+            'numTeams': '3',
+            'team1score': '300',
+            'team2score': '215',
+            'team3score': '25',
+            'team4score': None,
+            'targetScore': '0',
+            'onlineState': '',
+            'isRanked': 'true',
+            'hasPunkbuster': 'true',
+            'hasPassword': 'false',
+            'serverUptime': '5148',
+            'roundTime': '455',
+            'gameIpAndPort': '1.2.3.4:5445',
+            'punkBusterVersion': '1.5',
+            'joinQueueEnabled': 'false',
+            'region': 'EU',
+            'closestPingSite': '45',
+            'country': 'FR',
+        }, Bf3Parser.decodeServerinfo(('BigBrotherBot #2', '0', '16', 'ConquestLarge0', 'MP_012', '1', '2', '3', '300', '215', '25', '0', '', 'true', 'true', 'false', '5148', '455', '1.2.3.4:5445', '1.5', 'false', 'EU', '45', 'FR')))
+
+        self.assertDictEqual({
+            'serverName': 'BigBrotherBot #2',
+            'numPlayers': '0',
+            'maxPlayers': '16',
+            'gamemode': 'ConquestLarge0',
+            'level': 'MP_012',
+            'roundsPlayed': '1',
+            'roundsTotal': '2',
+            'numTeams': '4',
+            'team1score': '300',
+            'team2score': '215',
+            'team3score': '25',
+            'team4score': '84',
+            'targetScore': '0',
+            'onlineState': '',
+            'isRanked': 'true',
+            'hasPunkbuster': 'true',
+            'hasPassword': 'false',
+            'serverUptime': '5148',
+            'roundTime': '455',
+            'gameIpAndPort': '1.2.3.4:5445',
+            'punkBusterVersion': '1.5',
+            'joinQueueEnabled': 'false',
+            'region': 'EU',
+            'closestPingSite': '45',
+            'country': 'FR',
+        }, Bf3Parser.decodeServerinfo(('BigBrotherBot #2', '0', '16', 'ConquestLarge0', 'MP_012', '1', '2', '4', '300', '215', '25', '84', '0', '', 'true', 'true', 'false', '5148', '455', '1.2.3.4:5445', '1.5', 'false', 'EU', '45', 'FR')))
+
+    def test_getServerInfo_pre_R9(self):
+        self.maxDiff = None
+        bf3_response = ('BigBrotherBot #2', '0', '16', 'ConquestLarge0', 'MP_012', '1', '2',
+                                  '4', '300', '215', '25', '84',
+                                  '0', '', 'true', 'true', 'false', '5148', '455')
+        parser = Mock(spec=Bf3Parser)
+        parser.write = lambda x: bf3_response
+
+        data = Bf3Parser.getServerInfo(parser)
+        self.assertEqual(bf3_response, data)
+        self.assertEqual(parser.game.sv_hostname, 'BigBrotherBot #2')
+        self.assertEqual(parser.game.sv_maxclients, 16)
+        self.assertEqual(parser.game.gameType, 'ConquestLarge0')
+        self.assertFalse(parser._publicIp.called)
+        self.assertFalse(parser._port.called)
+
+        self.assertEqual({
+            'serverName': 'BigBrotherBot #2',
+            'numPlayers': '0',
+            'maxPlayers': '16',
+            'gamemode': 'ConquestLarge0',
+            'level': 'MP_012',
+            'roundsPlayed': '1',
+            'roundsTotal': '2',
+            'numTeams': '4',
+            'team1score': '300',
+            'team2score': '215',
+            'team3score': '25',
+            'team4score': '84',
+            'targetScore': '0',
+            'onlineState': '',
+            'isRanked': 'true',
+            'hasPunkbuster': 'true',
+            'hasPassword': 'false',
+            'serverUptime': '5148',
+            'roundTime': '455',
+            'gameIpAndPort': None,
+            'punkBusterVersion': None,
+            'joinQueueEnabled': None,
+            'region': None,
+            'closestPingSite': None,
+            'country': None,
+        }, parser.game.serverinfo)
+
+    def test_getServerInfo_R9(self):
+
+        bf3_response = ['i3D.net - BigBrotherBot #3 (FR)', '0', '16', 'SquadDeathMatch0', 'MP_013',
+                        '0', '1','4', '0', '0', '0', '0', '50', '', 'true', 'true', 'false', '92480',
+                        '4832', '4.5.6.7:542', '', '', 'EU', 'ams', 'DE']
+        parser = Mock(spec=Bf3Parser)
+        parser.write = lambda x: bf3_response
+
+        data = Bf3Parser.getServerInfo(parser)
+        self.assertEqual(bf3_response, data)
+        self.assertEqual(parser.game.sv_hostname, 'i3D.net - BigBrotherBot #3 (FR)')
+        self.assertEqual(parser.game.sv_maxclients, 16)
+        self.assertEqual(parser.game.gameType, 'SquadDeathMatch0')
+        self.assertEqual(parser._publicIp, '4.5.6.7')
+        self.assertEqual(parser._gamePort, '542')
+        self.assertEqual({
+            'serverName': 'i3D.net - BigBrotherBot #3 (FR)',
+            'numPlayers': '0',
+            'maxPlayers': '16',
+            'gamemode': 'SquadDeathMatch0',
+            'level': 'MP_013',
+            'roundsPlayed': '0',
+            'roundsTotal': '1',
+            'numTeams': '4',
+            'team1score': '0',
+            'team2score': '0',
+            'team3score': '0',
+            'team4score': '0',
+            'targetScore': '50',
+            'onlineState': '',
+            'isRanked': 'true',
+            'hasPunkbuster': 'true',
+            'hasPassword': 'false',
+            'serverUptime': '92480',
+            'roundTime': '4832',
+            'gameIpAndPort': '4.5.6.7:542',
+            'punkBusterVersion': '',
+            'joinQueueEnabled': '',
+            'region': 'EU',
+            'closestPingSite': 'ams',
+            'country': 'DE',
+        }, parser.game.serverinfo)
