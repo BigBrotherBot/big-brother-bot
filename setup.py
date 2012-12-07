ccf1d7dd54ad29e98d8f161c31ff63aac00402c0
Courgette <courgette@bigbrotherbot.net>
2012-12-07 21:18:53 +0100

    prepare release 1.9.0dev27

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ try:
 except:
     has_py2exe = False
 
-b3version = "1.9.0dev26"
+b3version = "1.9.0dev27"
 
 # override egg_info command to copy the b3.egg-info/PKG-INFO file into the b3 directory
 class my_egg_info(orig_egg_info):
