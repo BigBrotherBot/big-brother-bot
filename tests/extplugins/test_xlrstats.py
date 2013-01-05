1f906c17cd1fc3eb2768b727537b364f5a94556e
Courgette <courgette@bigbrotherbot.net>
2013-01-05 18:11:48 +0100

    TESTS: add a test case for validating the behavior of the XLRstats plugin when loaded with the default config file

--- tests/extplugins/test_xlrstats.py
+++ tests/extplugins/test_xlrstats.py
@@ -16,9 +16,12 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
+import os
 from b3.config import XmlConfigParser
-from b3.extplugins.xlrstats import XlrstatsPlugin
+from b3.extplugins.xlrstats import XlrstatsPlugin, __file__ as xlrstats__file__
 from tests import B3TestCase
+
+DEFAULT_XLRSTATS_CONFIG_FILE = os.path.join(os.path.dirname(xlrstats__file__), 'conf/xlrstats.xml')
 
 
 class Test_xlrstats(B3TestCase):
@@ -57,6 +60,51 @@ class Test_xlrstats(B3TestCase):
         self.assertEqual('', self.p.webfrontUrl)
         self.assertEqual(0, self.p.webfrontConfigNr)
         self.assertTrue(self.p.keep_history)
+        self.assertFalse(self.p.onemaponly)
+        self.assertEqual(1, self.p.minlevel)
+        self.assertEqual(1000, self.p.defaultskill)
+        self.assertEqual(16, self.p.Kfactor_high)
+        self.assertEqual(4, self.p.Kfactor_low)
+        self.assertEqual(100, self.p.Kswitch_kills)
+        self.assertEqual(600, self.p.steepness)
+        self.assertEqual(0.05, self.p.suicide_penalty_percent)
+        self.assertEqual(0.1, self.p.tk_penalty_percent)
+        self.assertEqual(2, self.p.assist_timespan)
+        self.assertEqual(10, self.p.damage_assist_release)
+        self.assertEqual(70, self.p.prematch_maxtime)
+        self.assertFalse(self.p.announce)
+        self.assertTrue(self.p.keep_time)
+        self.assertTrue(self.p._defaultTableNames)
+        self.assertEqual('xlr_playerstats', self.p.playerstats_table)
+        self.assertEqual('xlr_weaponstats', self.p.weaponstats_table)
+        self.assertEqual('xlr_weaponusage', self.p.weaponusage_table)
+        self.assertEqual('xlr_bodyparts', self.p.bodyparts_table)
+        self.assertEqual('xlr_playerbody', self.p.playerbody_table)
+        self.assertEqual('xlr_opponents', self.p.opponents_table)
+        self.assertEqual('xlr_mapstats', self.p.mapstats_table)
+        self.assertEqual('xlr_playermaps', self.p.playermaps_table)
+        self.assertEqual('xlr_actionstats', self.p.actionstats_table)
+        self.assertEqual('xlr_playeractions', self.p.playeractions_table)
+        self.assertEqual('xlr_history_monthly', self.p.history_monthly_table)
+        self.assertEqual('xlr_history_weekly', self.p.history_weekly_table)
+        self.assertEqual('ctime', self.p.ctime_table)
+
+    def test_default_conf(self):
+        """
+        Test the behaviors expected when one starts the Xlrstats plugin with the default config file
+        """
+        # GIVEN
+        self.conf.load(DEFAULT_XLRSTATS_CONFIG_FILE)
+        # WHEN
+        self.p.onLoadConfig()
+        # THEN
+        self.assertFalse(self.p.silent)
+        self.assertTrue(self.p.hide_bots)
+        self.assertTrue(self.p.exclude_bots)
+        self.assertEqual(3, self.p.minPlayers)
+        self.assertEqual('', self.p.webfrontUrl)
+        self.assertEqual(0, self.p.webfrontConfigNr)
+        self.assertFalse(self.p.keep_history)
         self.assertFalse(self.p.onemaponly)
         self.assertEqual(1, self.p.minlevel)
         self.assertEqual(1000, self.p.defaultskill)
