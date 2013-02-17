2f42ecd350fac1733f31c00ac4f3ad87db14284d
Thomas LÉVEIL <thomasleveil@gmail.com>
2013-02-17 12:00:43 +0100

    resolve conflict rebasing onto master

--- tests/run.py
+++ tests/run.py
@@ -4,5 +4,10 @@ Eventual command line arguments will be passed over to nosetests.
 """
 
 if __name__ == '__main__':
-    import nose
-    nose.main()
+    import os
+    import sys
+    from nose.core import TestProgram
+
+    argv = ['nosetests', os.path.dirname(__file__)]
+    argv.extend(sys.argv)
+    TestProgram(argv=argv)
