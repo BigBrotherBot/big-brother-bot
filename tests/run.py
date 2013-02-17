fcb767ef491c82e70410de62767d5c0c6bb31b55
Thomas LÉVEIL <thomasleveil@gmail.com>
2013-02-17 12:00:43 +0100

    [TESTS] update script helper to run the test suite and improve the test documentation

--- tests/run.py
+++ tests/run.py
@@ -1,3 +1,13 @@
+"""
+Run the all the tests using nosetests.
+Eventual command line arguments will be passed over to nosetests.
+"""
+
 if __name__ == '__main__':
-    import nose
-    nose.main()
+    import os
+    import sys
+    from nose.core import TestProgram
+
+    argv = ['nosetests', os.path.dirname(__file__)]
+    argv.extend(sys.argv)
+    TestProgram(argv=argv)
