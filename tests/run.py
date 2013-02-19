c7d8236bdc75dd266efdb9cc70c4c1bcddfb0df9
Thomas LÉVEIL <thomasleveil@gmail.com>
2013-02-19 04:09:26 +0100

    [TESTS] revert 3ea4af6
    Conflicts:
    	tests/run.py

--- tests/run.py
+++ tests/run.py
@@ -4,10 +4,5 @@ Eventual command line arguments will be passed over to nosetests.
 """
 
 if __name__ == '__main__':
-    import os
-    import sys
-    from nose.core import TestProgram
-
-    argv = ['nosetests', os.path.dirname(__file__)]
-    argv.extend(sys.argv)
-    TestProgram(argv=argv)
+    import nose
+    nose.main()
