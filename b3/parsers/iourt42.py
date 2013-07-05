928fece81ad5483fb0aaf120ec112c05faaad8b9
Fenix <fenix@urbanterror.info>
2013-07-05 20:40:05 +0200

    Updated changelog

--- b3/parsers/iourt42.py
+++ b3/parsers/iourt42.py
@@ -59,7 +59,13 @@
 #       UT_MOD_* constants.
 # 08/01/2013 - 1.11.1 - Courgette
 #     * fix EVT_SURVIVOR_WIN event
+# 05/07/2012 - 1.12 - Fenix
+#     * added support for new UrT 4.2.013 weapons
+#     * aorrectly parse ClientJumpRunStarted and ClientJumpRunStopped
+#     * added parsing of ClientJumpRunCanceled (generate EVT_CLIENT_JUMP_RUN_CANCEL)
+#     * aixed Client(Load|Save)Position parsing
 #
+
 import re, new
 import time
 from b3.functions import time2minutes
