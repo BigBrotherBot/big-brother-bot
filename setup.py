6d5edbbe54043d9b2633c8700779fd3a6590f98c
Thomas LÉVEIL <thomasleveil@gmail.com>
2014-01-12 23:44:57 +0100

    [setup] typo fix which has no impact on the installation process

--- setup.py
+++ setup.py
@@ -108,7 +108,7 @@ if has_py2exe:
 setup(cmdclass=cmdclass,
     name="b3",
     version=b3version,
-    tests_requires=['nose>=1.0', 'nose-exclude', 'mockito', 'pysqlite'],
+    tests_require=['nose>=1.0', 'nose-exclude', 'mockito', 'pysqlite'],
     packages=find_packages(),
     extras_require={ 'mysql' : 'MySQL-python' },
     package_data={
