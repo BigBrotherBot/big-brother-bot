0c3dabc05435a73b507c62d635f051dac875c649
FenixXx <danielepantaleone@icloud.com>
2014-01-15 02:47:27 +0100

    wop: pep8 coding style guide

--- b3/parsers/wop.py
+++ b3/parsers/wop.py
@@ -24,37 +24,42 @@
 # 09/04/2011 - 1.0.3 - Courgette
 # * reflect that cid are not converted to int anymore in the clients module
 
-__author__  = 'xlr8or'
+__author__ = 'xlr8or'
 __version__ = '1.0.3'
 
-from b3.parsers.q3a.abstractParser import AbstractParser
-import re, string
+import re
+import string
 import b3
 import b3.events
+import b3.parsers.punkbuster
 
-#----------------------------------------------------------------------------------------------------------------------------------------------
+from b3.parsers.q3a.abstractParser import AbstractParser
+
+
 class WopParser(AbstractParser):
+
     gameName = 'wop'
     privateMsg = False
 
-    _settings = {}
-    _settings['line_length'] = 65
-    _settings['min_wrap_length'] = 100
+    _settings = dict(
+        line_length=65,
+        min_wrap_length=100,
+    )
 
-    _commands = {}
-    _commands['message'] = '%(prefix)s^7 %(message)s'
-    _commands['deadsay'] = '%(prefix)s [DEAD]^7 %(message)s'
-    _commands['say'] = 'say %(prefix)s^7 %(message)s'
+    _commands = dict(
+        message='%(prefix)s^7 %(message)s',
+        deadsay='%(prefix)s [DEAD]^7 %(message)s',
+        say='say %(prefix)s^7 %(message)s',
+        set='set %(name)s "%(value)s"',
+        kick='clientkick %(cid)s',
+        ban='addip %(cid)s',
+        tempban='clientkick %(cid)s',
+    )
 
-    _commands['set'] = 'set %(name)s "%(value)s"'
-    _commands['kick'] = 'clientkick %(cid)s'
-    _commands['ban'] = 'addip %(cid)s'
-    _commands['tempban'] = 'clientkick %(cid)s'
-
-    _eventMap = {
-        'warmup' : b3.events.EVT_GAME_WARMUP,
-        'shutdowngame' : b3.events.EVT_GAME_ROUND_END
-    }
+    _eventMap = dict(
+        warmup=b3.events.EVT_GAME_WARMUP,
+        shutdowngame=b3.events.EVT_GAME_ROUND_END
+    )
 
     # remove the time off of the line
     _lineClear = re.compile(r'^(?:[0-9:]+\s?)?')
@@ -89,52 +94,56 @@ class WopParser(AbstractParser):
     _regPlayer = re.compile(r'^(?P<slot>[0-9]+)\s+(?P<score>[0-9-]+)\s+(?P<ping>[0-9]+)\s+(?P<name>.*?)\s+(?P<last>[0-9]+)\s+(?P<ip>[0-9.]+):(?P<port>[0-9-]+)\s+(?P<qport>[0-9]+)\s+(?P<rate>[0-9]+)$', re.I)
     _reColor = re.compile(r'(\^.)|[\x00-\x20]|[\x7E-\xff]')
 
-    PunkBuster = None
+    punk_buster = None
+
+    _clientConnectID = None
+    _clientConnectGuid = None
+    _clientConnectIp = None
 
     #kill modes
-    MOD_UNKNOWN='0'
-    MOD_SHOTGUN='1'
-    MOD_GAUNTLET='2'
-    MOD_MACHINEGUN='3'
-    MOD_GRENADE='4'
-    MOD_GRENADE_SPLASH='5'
-    MOD_ROCKET='6'
-    MOD_ROCKET_SPLASH='7'
-    MOD_PLASMA='8'
-    MOD_PLASMA_SPLASH='9'
-    MOD_RAILGUN='10'
-    MOD_LIGHTNING='11'
-    MOD_BFG='12'
-    MOD_BFG_SPLASH='13'
-    MOD_KILLERDUCKS='14'
-    MOD_WATER='15'
-    MOD_SLIME='16'
-    MOD_LAVA='17'
-    MOD_CRUSH='18'
-    MOD_TELEFRAG='19'
-    MOD_FALLING='20' # not used in wop
-    MOD_SUICIDE='21'
-    MOD_TARGET_LASER='22' # not used in wop
-    MOD_TRIGGER_HURT='23'
-    MOD_GRAPPLE='24' # not used in wop
-
+    MOD_UNKNOWN = '0'
+    MOD_SHOTGUN = '1'
+    MOD_GAUNTLET = '2'
+    MOD_MACHINEGUN = '3'
+    MOD_GRENADE = '4'
+    MOD_GRENADE_SPLASH = '5'
+    MOD_ROCKET = '6'
+    MOD_ROCKET_SPLASH = '7'
+    MOD_PLASMA = '8'
+    MOD_PLASMA_SPLASH = '9'
+    MOD_RAILGUN = '10'
+    MOD_LIGHTNING = '11'
+    MOD_BFG = '12'
+    MOD_BFG_SPLASH = '13'
+    MOD_KILLERDUCKS = '14'
+    MOD_WATER = '15'
+    MOD_SLIME = '16'
+    MOD_LAVA = '17'
+    MOD_CRUSH = '18'
+    MOD_TELEFRAG = '19'
+    MOD_FALLING = '20'  # not used in wop
+    MOD_SUICIDE = '21'
+    MOD_TARGET_LASER = '22'  # not used in wop
+    MOD_TRIGGER_HURT = '23'
+    MOD_GRAPPLE = '24'  # not used in wop
 
     def startup(self):
         # add the world client
-        client = self.clients.newClient('-1', guid='WORLD', name='World', hide=True, pbid='WORLD')
+        self.clients.newClient('-1', guid='WORLD', name='World', hide=True, pbid='WORLD')
 
         if not self.config.has_option('server', 'punkbuster') or self.config.getboolean('server', 'punkbuster'):
-            self.PunkBuster = b3.parsers.punkbuster.PunkBuster(self)
+            self.punk_buster = b3.parsers.punkbuster.PunkBuster(self)
 
         # get map from the status rcon command
-        map = self.getMap()
-        if map:
-            self.game.mapName = map
-            self.info('map is: %s'%self.game.mapName)
+        _map = self.getMap()
+        if _map:
+            self.game.mapName = _map
+            self.info('map is: %s' % self.game.mapName)
 
     def getLineParts(self, line):
         line = re.sub(self._lineClear, '', line, 1)
 
+        m = None
         for f in self._lineFormats:
             m = re.match(f, line)
             if m:
@@ -144,37 +153,37 @@ class WopParser(AbstractParser):
         if m:
             client = None
             target = None
-            return (m, m.group('action').lower(), m.group('data').strip(), client, target)
+            return m, m.group('action').lower(), m.group('data').strip(), client, target
         else:
             self.verbose('line did not match format: %s' % line)
 
     def parseUserInfo(self, info):
-        #3 n\Dr.Schraube\t\0\model\padman/padsoldier_red\hmodel\padman/padsoldier_red\c1\4\c2\1\hc\100\w\0\l\0\tt\0\tl\0\sl\
-        playerID, info = string.split(info, ' ', 1)
+        #3 n\Dr.Schraube\t\0\model\padman/padsoldier_red\hmodel\padman/padsoldier_red\c1\4\c2\1\hc\100\w\0\l\0\tt\...
+        player_id, info = string.split(info, ' ', 1)
 
         if info[:1] != '\\':
             info = '\\' + info
 
         options = re.findall(r'\\([^\\]+)\\([^\\]+)', info)
 
-        data = {}
+        data = dict()
         for o in options:
             data[o[0]] = o[1]
 
-        data['cid'] = playerID
+        data['cid'] = player_id
 
-        if data.has_key('n'):
+        if 'n' in data.keys():
             data['name'] = data['n']
 
         t = 0
-        if data.has_key('team'):
+        if 'team' in data.keys():
             t = data['team']
-        elif data.has_key('t'):
+        elif 't' in data.keys():
             t = data['t']
 
         data['team'] = self.getTeam(t)
 
-        if data.has_key('cl_guid') and not data.has_key('pbid'):
+        if 'cl_guid' in data.keys() and not 'pbid' in data.keys():
             data['pbid'] = data['cl_guid']
 
         return data
@@ -182,35 +191,41 @@ class WopParser(AbstractParser):
     def OnClientconnect(self, action, data, match=None):
         # we get user info in two parts:
         # ClientConnect: 2 77F303414E4355E0860B483F2A07E4DF 151.16.71.226:27960
-        # ClientUserinfoChanged: 2 n\^3Ghost^2Pirate\t\0\model\piratpad/ghostpirate_red\hmodel\piratpad/ghostpirate_red\c1\4\c2\0\hc\70\w\0\l\0\skill\    2.00\tt\0\tl\0\sl\
-        # we need to store the ClientConnect ID, the guid and IP for the next call to Clientuserinfochanged only on initial connection
+        # ClientUserinfoChanged: 2 n\^3Ghost^2Pirate\t\0\model\piratpad/ghostpirate_red\hmodel\piratpad...
+        # we need to store the ClientConnect ID, the guid and IP for the next call to
+        # Clientuserinfochanged only on initial connection
 
         try:
-            self._clientConnectID = match.group('cid') # Normal client connected
-        except:
+            # Normal client connected
+            self._clientConnectID = match.group('cid')
+        except IndexError:
+
             try:
-                self._clientConnectID = match.group('bcid') # Game Bot identifier
+                # Game Bot identifier
+                self._clientConnectID = match.group('bcid')
                 self._clientConnectGuid = 'BOT' + str(match.group('bcid'))
                 self._clientConnectIp = '0.0.0.0'
                 self.bot('Bot Connected')
                 return None
-            except:
+            except IndexError:
                 self.error('Parser could not connect client')
                 return None
 
         try:
-            self._clientConnectGuid = match.group('cl_guid') # If we have no cl_guid we'll use the ip instead.
-        except:
+            # If we have no cl_guid we'll use the ip instead.
+            self._clientConnectGuid = match.group('cl_guid')
+        except IndexError:
             self._clientConnectGuid = match.group('ip')
 
         self._clientConnectIp = match.group('ip')
-        self.verbose('Client Connected cid: %s, guid: %s, ip: %s' % (self._clientConnectID, self._clientConnectGuid, self._clientConnectIp))
+        self.verbose('Client Connected cid: %s, guid: %s, ip: %s' % (self._clientConnectID,
+                                                                     self._clientConnectGuid,
+                                                                     self._clientConnectIp))
 
     def OnClientuserinfochanged(self, action, data, match=None):
-        try:
-            id = self._clientConnectID
-        except:
-            id = None # We've already connected before
+        _id = None
+        if self._clientConnectID is not None:
+            _id = self._clientConnectID
 
         self._clientConnectID = None
 
@@ -219,7 +234,7 @@ class WopParser(AbstractParser):
         if bclient:
             client = self.clients.getByCID(bclient['cid'])
 
-            if id:
+            if _id:
                 bclient['cl_guid'] = self._clientConnectGuid
                 self._clientConnectGuid = None
                 bclient['ip'] = self._clientConnectIp
@@ -232,10 +247,10 @@ class WopParser(AbstractParser):
                 for k, v in bclient.iteritems():
                     setattr(client, k, v)
             else:
-                #make a new client
+                # make a new client
                 client = self.clients.newClient(bclient['cid'], name=bclient['name'], ip=bclient['ip'], state=b3.STATE_ALIVE, guid=bclient['cl_guid'], data={ 'guid' : bclient['cl_guid'] })
 
-        if id:
+        if _id:
             return b3.events.Event(b3.events.EVT_CLIENT_JOIN, None, client)
         else:
             return None
@@ -327,7 +342,7 @@ class WopParser(AbstractParser):
         23:   MOD_TRIGGER_HURT, Killed by a trigger_hurt
         24:   MOD_GRAPPLE, Killed by grapple, not used in WoP
         """
-        self.debug('OnKill: %s (%s)'%(match.group('aweap'),match.group('text')))
+        self.debug('OnKill: %s (%s)' % (match.group('aweap'), match.group('text')))
         
         victim = self.clients.getByCID(match.group('cid'))
         if not victim:
@@ -341,7 +356,7 @@ class WopParser(AbstractParser):
             return None
 
         ## Fix attacker
-        if match.group('aweap') in (self.MOD_WATER,self.MOD_LAVA,self.MOD_FALLING,self.MOD_TRIGGER_HURT,):
+        if match.group('aweap') in (self.MOD_WATER, self.MOD_LAVA, self.MOD_FALLING, self.MOD_TRIGGER_HURT,):
             # those kills should be considered suicides
             self.debug('OnKill: water/lava/falling/trigger_hurt should be suicides')
             attacker = victim
@@ -353,8 +368,8 @@ class WopParser(AbstractParser):
             self.debug('No attacker')
             return None
 
-        dType = match.group('text').split()[-1:][0]
-        if not dType:
+        dtype = match.group('text').split()[-1:][0]
+        if not dtype:
             self.debug('No damageType, weapon: %s' % weapon)
             return None
 
@@ -363,10 +378,8 @@ class WopParser(AbstractParser):
         # fix event for team change and suicides and tk
         if attacker.cid == victim.cid:
             if weapon == self.MOD_CHANGE_TEAM:
-                """
-                Do not pass a teamchange event here. That event is passed
-                shortly after the kill.
-                """
+                # Do not pass a teamchange event here. That event is passed
+                # shortly after the kill.
                 self.verbose('Team Change Event Caught, exiting')
                 return None
             else:
@@ -379,9 +392,10 @@ class WopParser(AbstractParser):
             victim.hitloc = 'body'
         
         victim.state = b3.STATE_DEAD
-        #self.verbose('OnKill Victim: %s, Attacker: %s, Weapon: %s, Hitloc: %s, dType: %s' % (victim.name, attacker.name, weapon, victim.hitloc, dType))
+        #self.verbose('OnKill Victim: %s, Attacker: %s, Weapon: %s, Hitloc: %s, dType: %s' % (
+        # victim.name, attacker.name, weapon, victim.hitloc, dType))
         # need to pass some amount of damage for the teamkill plugin - 100 is a kill
-        return b3.events.Event(event, (100, weapon, victim.hitloc, dType), attacker, victim)
+        return b3.events.Event(event, (100, weapon, victim.hitloc, dtype), attacker, victim)
 
     # item
     def OnItem(self, action, data, match=None):
@@ -395,31 +409,29 @@ class WopParser(AbstractParser):
 
     # Translate the gameType to a readable format
     # //WoP gametypes: 0=FFA / 1=1v1 / 2=SP / 3=SYC-FFA / 4=LPS / 5=TDM / 6=CTL / 7=SYC-TP / 8=BB
-    def defineGameType(self, gameTypeInt):
+    def defineGameType(self, gametype_int):
 
-        _gameType = ''
-        _gameType = str(gameTypeInt)
-        #self.debug('gameTypeInt: %s' % gameTypeInt)
+        _gametype = str(gametype_int)
+        #self.debug('gametype_int: %s' % gametype_int)
         
-        if gameTypeInt == '0':
-            _gameType = 'dm'
-        elif gameTypeInt == '1':
-            _gameType = 'lvl'
-        elif gameTypeInt == '2':
-            _gameType = 'sp'
-        elif gameTypeInt == '3':
-            _gameType = 'syc-ffa'
-        elif gameTypeInt == '4':
-            _gameType = 'lps'
-        elif gameTypeInt == '5':
-            _gameType = 'tdm'
-        elif gameTypeInt == '6':
-            _gameType = 'ctl'
-        elif gameTypeInt == '7':
-            _gameType = 'syc-tp'
-        elif gameTypeInt == '8':
-            _gameType = 'bb'
+        if gametype_int == '0':
+            _gametype = 'dm'
+        elif gametype_int == '1':
+            _gametype = 'lvl'
+        elif gametype_int == '2':
+            _gametype = 'sp'
+        elif gametype_int == '3':
+            _gametype = 'syc-ffa'
+        elif gametype_int == '4':
+            _gametype = 'lps'
+        elif gametype_int == '5':
+            _gametype = 'tdm'
+        elif gametype_int == '6':
+            _gametype = 'ctl'
+        elif gametype_int == '7':
+            _gametype = 'syc-tp'
+        elif gametype_int == '8':
+            _gametype = 'bb'
         
-        #self.debug('_gameType: %s' % _gameType)
-        return _gameType
-
+        #self.debug('_gametype: %s' % _gametype)
+        return _gametype
