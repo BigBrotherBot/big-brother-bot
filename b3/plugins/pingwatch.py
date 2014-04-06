e2f3555f9d87bcfc40d9f32b6d5a019d32a11e60
FenixXx <danielepantaleone@icloud.com>
2014-04-06 19:04:33 +0200

    [PINGWATCH] pep8 coding style guide + minor improvements

--- b3/plugins/pingwatch.py
+++ b3/plugins/pingwatch.py
@@ -18,27 +18,27 @@
 #
 # CHANGELOG
 #    04/23/2010 - 1.2.2 - Bakes
-#    Fix to !ci after bug reported by sgt
+#       Fix to !ci after bug reported by sgt
 #    03/28/2010 - 1.2.1 - xlr8or
-#    add default _minLevel for !ci if config option is missing
+#       Add default _minLevel for !ci if config option is missing
 #    03/21/2010 - 1.2.0 - Bakes
-#    Added the !ci command from the admin plugin.
+#       Added the !ci command from the admin plugin.
 #    11/30/2005 - 1.1.3 - ThorN
-#    Use PluginCronTab instead of CronTab
+#       Use PluginCronTab instead of CronTab
 #    8/29/2005 - 1.1.0 - ThorN
-#    Converted to use new event handlers
+#       Converted to use new event handlers
 
-__author__  = 'ThorN'
+__author__ = 'ThorN'
 __version__ = '1.2.2'
 
 
-
-import b3, time, thread
+import b3
 import b3.events
 import b3.plugin
 import b3.cron
+from ConfigParser import NoOptionError
 
-#--------------------------------------------------------------------------------------------------
+
 class PingwatchPlugin(b3.plugin.Plugin):
     _interval = 0
     _maxPing = 0
@@ -49,15 +49,19 @@ class PingwatchPlugin(b3.plugin.Plugin):
     _minLevel = 20
 
     def onStartup(self):
-        self.registerEvent(b3.events.EVT_GAME_EXIT)
-
-        # dont check pings on startup
-        self._ignoreTill = self.console.time() + 120
+        """\
+        Initialize plugin
+        """
+        self.registerEvent(self.console.getEventID('EVT_GAME_EXIT'), self.onGameExit)
+        self._ignoreTill = self.console.time() + 120  # dont check pings on startup
 
     def onLoadConfig(self):
+        """\
+        Load plugin configuration
+        """
         try:
             self._minLevel = self.config.get('commands', 'ci')
-        except:
+        except NoOptionError:
             pass 
 
         self._interval = self.config.getint('settings', 'interval')
@@ -73,47 +77,62 @@ class PingwatchPlugin(b3.plugin.Plugin):
         self._adminPlugin = self.console.getPlugin('admin')
         self._adminPlugin.registerCommand(self, 'ci', self._minLevel, self.cmd_ci)
 
-    def onEvent(self, event):
-        if event.type == b3.events.EVT_GAME_EXIT:
-            # ignore ping watching for 2 minutes
-            self._ignoreTill = self.console.time() + 120
+    def onGameExit(self, event):
+        """\
+        Handle intercepted events
+        """
+        # ignore ping watching for 2 minutes
+        self._ignoreTill = self.console.time() + 120
 
-    def check(self):        
-        if self.isEnabled() and (self.console.time() > self._ignoreTill):        
-            for cid,ping in self.console.getPlayerPings().items():
-                #self.console.verbose('ping %s = %s', cid, ping)
-                if ping > self._maxPing:                
-                    client = self.console.clients.getByCID(cid)
-                    if client:
-                        if client.var(self, 'highping', 0).value > 0:
-                            self.console.verbose('set high ping check %s = %s (%s)', cid, ping, client.var(self, 'highping', 0).value)
-                            if self.console.time() - client.var(self, 'highping', 0).value > self._maxPingDuration:
-                                if ping == 999:                            
-                                    #client.kick('^7Connection interupted')
-                                    self.console.say('^7%s ^7ping detected as Connection Interrupted (CI)' % client.name)
-                                else:
-                                    #client.kick('^7Ping too high %s' % ping)
-                                    self.console.say('^7%s ^7ping detected as too high %s' % (client.name, ping))
-                        else:
-                            self.console.verbose('set ping watch %s = %s', cid, ping)
-                            client.setvar(self, 'highping', self.console.time())
+    def check(self):
+        """\
+        Check for clients with high ping
+        """
+        if not self.isEnabled() or self.console.time() <= self._ignoreTill:
+            # we are not supposed to check
+            return
+
+        for cid, ping in self.console.getPlayerPings().items():
+            # loop through all the connected clients
+            # self.console.verbose('ping %s = %s', cid, ping)
+            if ping <= self._maxPing:
+                continue
+
+            client = self.console.clients.getByCID(cid)
+            if not client:
+                continue
+
+            if not client.isvar(self, 'highping'):
+                self.console.verbose('set ping watch %s = %s', cid, ping)
+                client.setvar(self, 'highping', self.console.time())
+                return
+
+            self.console.verbose('set high ping check %s = %s (%s)', cid, ping, client.var(self, 'highping', 0).value)
+            if self.console.time() - client.var(self, 'highping', 0).value > self._maxPingDuration:
+                if ping == 999:
+                    self.console.say('^7%s ^7ping detected as Connection Interrupted (CI)' % client.name)
+                else:
+                    self.console.say('^7%s ^7ping detected as too high %s' % (client.name, ping))
 
     def cmd_ci(self, data, client=None, cmd=None):
         """\
-        <player> - Kick a player that has an interrupted connection
+        <client> - Kick a client that has an interrupted connection
         """
-
         m = self._adminPlugin.parseUserCmd(data)
         if not m:
-            client.message('^7Invalid parameters, you must supply a player name')
+            client.message('^7Missing data, try !help ci')
             return False
 
         sclient = self._adminPlugin.findClientPrompt(m[0], client)
-        if sclient:
-            for cid,ping in self.console.getPlayerPings().items():
-                if cid == sclient.cid:
-                    if ping > self._maxCiPing:
-                        sclient.kick(self._adminPlugin.getReason('ci'), 'ci', client)
-                    else:
-                        client.message('^7%s ^7is not CI' % sclient.exactName)
-                    break
+        if not sclient:
+            return
+
+        try:
+            players = self.console.getPlayerPings()
+            ping = players[str(sclient.cid)]
+            if ping > self._maxCiPing:
+                sclient.kick(self._adminPlugin.getReason('ci'), 'ci', client)
+            else:
+                client.message('^7%s ^7is not CI' % sclient.exactName)
+        except KeyError:
+            pass
