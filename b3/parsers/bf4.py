7b1b935c40118a75d7df141779d73e0138afb4b3
Harry Gabriel <rootdesign@gmail.com>
2014-10-26 15:05:27 +0100

    [BF4] update to server version R43
    - add new vars
    - update version

--- b3/parsers/bf4.py
+++ b3/parsers/bf4.py
@@ -41,7 +41,7 @@ from b3.parsers.frostbite2.util import PlayerInfoBlock
 __author__ = 'Courgette, ozon, Dwarfer'
 __version__ = '1.1'
 
-BF4_REQUIRED_VERSION = 135082
+BF4_REQUIRED_VERSION = 150767
 
 BF4_PLAYER = 0              # normal player
 BF4_SPECTATOR = 1           # spectator which is not visible in the game for other player but visible as player for b3
@@ -505,6 +505,9 @@ class Bf4Parser(AbstractParser):
         'preset',                   # allows you to set the server to either normal, hardcore infantry or custom
         'unlockMode'                # which set of unlocks is available on an unranked server (all/common/stats/none)
         'ticketBleedRate'           # controls the rate of tickets drop
+        'roundPlayersReadyBypassTimer'  # <time> amount of seconds after which the ready screen is bypassed
+        'roundPlayersReadyMinCount'     # <count> minimum number of players per team which have to confirm ready state
+        'roundPlayersReadyPercent'  # <modifier:percent>
     )
 
     # gamemodes aliases {alias: actual game mode name}
