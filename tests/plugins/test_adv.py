140f287075f1c55b1e7278e7dd750fbf8a92bcb1
FenixXx <danielepantaleone@me.com>
2015-01-30 13:34:41 +0100

    [TESTS] updated plugin adv tests to load .ini configuration file format

--- tests/plugins/test_adv.py
+++ tests/plugins/test_adv.py
@@ -16,25 +16,27 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
-import StringIO
+
+import unittest2 as unittest
 import logging
 import os
+
 from mock import patch, call, Mock
 from mockito import when, any as mockito_any, unstub
+from tests import B3TestCase
+from textwrap import dedent
+
 from b3.fake import FakeClient
 from b3.lib import feedparser
 from b3.plugins.admin import AdminPlugin
-from tests import B3TestCase
-import unittest2 as unittest
-
 from b3.plugins.adv import AdvPlugin, MessageLoop
 from b3.config import XmlConfigParser, CfgConfigParser
-
 from b3 import __file__ as b3_module__file__
+
 ADMIN_CONFIG_FILE = os.path.normpath(os.path.join(os.path.dirname(b3_module__file__), "conf/plugin_admin.ini"))
 ADMIN_CONFIG = None
 
-default_plugin_file = os.path.normpath(os.path.join(os.path.dirname(__file__), "../../b3/conf/plugin_adv.xml"))
+default_plugin_file = os.path.normpath(os.path.join(os.path.dirname(__file__), "../../b3/conf/plugin_adv.ini"))
 default_plugin_content = None
 
 timer_patcher = None
@@ -87,11 +89,11 @@ class AdvTestCase(B3TestCase):
     def init_plugin(self, config_content=None):
         conf = None
         if config_content:
-            conf = XmlConfigParser()
-            conf.setXml(config_content)
+            conf = CfgConfigParser(allow_no_value=True)
+            conf.loadFromString(config_content)
         elif default_plugin_content:
-            conf = XmlConfigParser()
-            conf.setXml(default_plugin_content)
+            conf = CfgConfigParser(allow_no_value=True)
+            conf.loadFromString(default_plugin_content)
         else:
             unittest.skip("cannot get default plugin config file at %s" % default_plugin_file)
 
@@ -101,7 +103,7 @@ class AdvTestCase(B3TestCase):
         self.log.setLevel(logging.DEBUG)
         self.log.info("============================= Adv plugin: loading config ============================")
         self.p.onLoadConfig()
-        self.log.info("============================= Adv plugin: starting  =================================")
+        self.log.info("================================ Adv plugin: starting  ==============================")
         self.p.onStartup()
 
 
@@ -114,16 +116,15 @@ class Test_default_config(AdvTestCase):
         self.assertIsNone(self.p._fileName)
         self.assertEqual(10, len(self.p._msg.items))
         self.assertEqual("http://forum.bigbrotherbot.net/news-2/?type=rss;action=.xml", self.p._feed)
-        self.assertEqual("News: ", self.p._feedpre)
+        self.assertEqual("News:", self.p._feedpre)
         self.assertEqual(4, self.p._feedmaxitems)
-        self.assertEqual('News: ', self.p._feedpre)
         self.assertIsNotNone(self.p._cronTab)
         self.assertTupleEqual((0, range(0, 59, 2), -1, -1, -1, -1),
                               (self.p._cronTab.second, self.p._cronTab.minute, self.p._cronTab.hour,
                                self.p._cronTab.day, self.p._cronTab.month, self.p._cronTab.dow))
 
     def test_empty(self):
-        self.init_plugin("""<configuration plugin="adv" />""")
+        self.init_plugin(dedent(r"""[settings]"""))
         self.assertEqual(self.p._rate, '2')
         self.assertIsNone(self.p._fileName)
         self.assertEqual(0, len(self.p._msg.items))
@@ -134,13 +135,10 @@ class Test_default_config(AdvTestCase):
         self.assertIsNotNone(self.p._cronTab)
 
     def test_rate_nominal(self):
-        self.init_plugin("""\
-<configuration plugin="adv">
-    <settings name="settings">
-        <set name="rate">1</set>
-    </settings>
-</configuration>
-""")
+        self.init_plugin(dedent(r"""
+            [settings]
+            rate: 1
+        """))
         self.assertEqual('1', self.p._rate)
         self.assertIsNotNone(self.p._cronTab)
         self.assertTupleEqual((0, range(60), -1, -1, -1, -1),
@@ -148,13 +146,10 @@ class Test_default_config(AdvTestCase):
                                self.p._cronTab.day, self.p._cronTab.month, self.p._cronTab.dow))
 
     def test_rate_nominal_second(self):
-        self.init_plugin("""\
-<configuration plugin="adv">
-    <settings name="settings">
-        <set name="rate">40s</set>
-    </settings>
-</configuration>
-""")
+        self.init_plugin(dedent(r"""
+            [settings]
+            rate: 40s
+        """))
         self.assertEqual('40s', self.p._rate)
         self.assertIsNotNone(self.p._cronTab)
         self.assertTupleEqual(([0, 40], -1, -1, -1, -1, -1),
@@ -163,13 +158,10 @@ class Test_default_config(AdvTestCase):
 
     def test_rate_junk(self):
         try:
-            self.init_plugin("""\
-<configuration plugin="adv">
-    <settings name="settings">
-        <set name="rate">f00</set>
-    </settings>
-</configuration>
-""")
+            self.init_plugin(dedent(r"""
+            [settings]
+            rate: f00
+        """))
         except TypeError, err:
             print err
         except Exception:
@@ -190,49 +182,37 @@ class Test_commands(AdvTestCase):
 
     #################### advlist ####################
     def test_advlist_empty(self):
-        self.init_plugin("""
-            <configuration>
-                <settings name="settings">
-                    <set name="rate">30s</set>
-                </settings>
-                <ads>
-                </ads>
-            </configuration>
-        """)
+        self.init_plugin(dedent(r"""
+            [settings]
+            rate: 30s
+            [ads]
+        """))
         self.joe.clearMessageHistory()
         self.p.cmd_advlist(data=None, client=self.joe)
         self.assertEqual([], self.p._msg.items)
         self.assertEqual(['Adv: no ads loaded'], self.joe.message_history)
 
     def test_advlist_one_item(self):
-        self.init_plugin("""
-            <configuration>
-                <settings name="settings">
-                    <set name="rate">30s</set>
-                </settings>
-                <ads>
-                    <ad>f00</ad>
-                </ads>
-            </configuration>
-        """)
+        self.init_plugin(dedent(r"""
+            [settings]
+            rate: 30s
+            [ads]
+            f00
+        """))
         self.joe.clearMessageHistory()
         self.p.cmd_advlist(data=None, client=self.joe)
         self.assertEqual(['f00'], self.p._msg.items)
         self.assertEqual(['Adv: [1] f00'], self.joe.message_history)
 
     def test_advlist_many_items(self):
-        self.init_plugin("""
-            <configuration>
-                <settings name="settings">
-                    <set name="rate">30s</set>
-                </settings>
-                <ads>
-                    <ad>f00</ad>
-                    <ad>bar</ad>
-                    <ad>test</ad>
-                </ads>
-            </configuration>
-        """)
+        self.init_plugin(dedent(r"""
+            [settings]
+            rate: 30s
+            [ads]
+            f00
+            bar
+            test
+        """))
         self.joe.clearMessageHistory()
         self.p.cmd_advlist(data=None, client=self.joe)
         self.assertEqual(['f00', 'bar', 'test'], self.p._msg.items)
@@ -241,54 +221,42 @@ class Test_commands(AdvTestCase):
     #################### advrate ####################
 
     def test_advrate_no_arg_30s(self):
-        self.init_plugin("""
-            <configuration>
-                <settings name="settings">
-                    <set name="rate">30s</set>
-                </settings>
-                <ads>
-                    <ad>f00</ad>
-                    <ad>bar</ad>
-                    <ad>test</ad>
-                </ads>
-            </configuration>
-        """)
+        self.init_plugin(dedent(r"""
+            [settings]
+            rate: 30s
+            [ads]
+            f00
+            bar
+            test
+        """))
         self.joe.clearMessageHistory()
         self.p.cmd_advrate(data='', client=self.joe)
         self.assertEqual('30s', self.p._rate)
         self.assertEqual(['Current rate is every 30 seconds'], self.joe.message_history)
 
     def test_advrate_no_arg_2min(self):
-        self.init_plugin("""
-            <configuration>
-                <settings name="settings">
-                    <set name="rate">2</set>
-                </settings>
-                <ads>
-                    <ad>f00</ad>
-                    <ad>bar</ad>
-                    <ad>test</ad>
-                </ads>
-            </configuration>
-        """)
+        self.init_plugin(dedent(r"""
+            [settings]
+            rate: 2
+            [ads]
+            f00
+            bar
+            test
+        """))
         self.joe.clearMessageHistory()
         self.p.cmd_advrate(data=None, client=self.joe)
         self.assertEqual('2', self.p._rate)
         self.assertEqual(['Current rate is every 2 minutes'], self.joe.message_history)
 
     def test_advrate_set_20s(self):
-        self.init_plugin("""
-            <configuration>
-                <settings name="settings">
-                    <set name="rate">45s</set>
-                </settings>
-                <ads>
-                    <ad>f00</ad>
-                    <ad>bar</ad>
-                    <ad>test</ad>
-                </ads>
-            </configuration>
-        """)
+        self.init_plugin(dedent(r"""
+            [settings]
+            rate: 45s
+            [ads]
+            f00
+            bar
+            test
+        """))
         self.assertEqual('45s', self.p._rate)
         self.joe.clearMessageHistory()
         self.p.cmd_advrate(data="20s", client=self.joe)
@@ -296,18 +264,14 @@ class Test_commands(AdvTestCase):
         self.assertEqual(['Adv: rate set to 20 seconds'], self.joe.message_history)
 
     def test_advrate_set_3min(self):
-        self.init_plugin("""
-            <configuration>
-                <settings name="settings">
-                    <set name="rate">45s</set>
-                </settings>
-                <ads>
-                    <ad>f00</ad>
-                    <ad>bar</ad>
-                    <ad>test</ad>
-                </ads>
-            </configuration>
-        """)
+        self.init_plugin(dedent(r"""
+            [settings]
+            rate: 45s
+            [ads]
+            f00
+            bar
+            test
+        """))
         self.assertEqual('45s', self.p._rate)
         self.joe.clearMessageHistory()
         self.p.cmd_advrate(data="3", client=self.joe)
@@ -317,18 +281,14 @@ class Test_commands(AdvTestCase):
     #################### advrem ####################
 
     def test_advrem_nominal(self):
-        self.init_plugin("""
-            <configuration>
-                <settings name="settings">
-                    <set name="rate">45s</set>
-                </settings>
-                <ads>
-                    <ad>f00</ad>
-                    <ad>bar</ad>
-                    <ad>test</ad>
-                </ads>
-            </configuration>
-        """)
+        self.init_plugin(dedent(r"""
+            [settings]
+            rate: 45s
+            [ads]
+            f00
+            bar
+            test
+        """))
         self.assertEqual(['f00', 'bar', 'test'], self.p._msg.items)
         self.joe.clearMessageHistory()
         self.p.cmd_advrem(data="2", client=self.joe)
@@ -336,18 +296,14 @@ class Test_commands(AdvTestCase):
         self.assertEqual(['Adv: removed item: bar'], self.joe.message_history)
 
     def test_advrem_no_arg(self):
-        self.init_plugin("""
-            <configuration>
-                <settings name="settings">
-                    <set name="rate">45s</set>
-                </settings>
-                <ads>
-                    <ad>f00</ad>
-                    <ad>bar</ad>
-                    <ad>test</ad>
-                </ads>
-            </configuration>
-        """)
+        self.init_plugin(dedent(r"""
+            [settings]
+            rate: 45s
+            [ads]
+            f00
+            bar
+            test
+        """))
         self.assertEqual(['f00', 'bar', 'test'], self.p._msg.items)
         self.joe.clearMessageHistory()
         self.p.cmd_advrem(data=None, client=self.joe)
@@ -355,18 +311,14 @@ class Test_commands(AdvTestCase):
         self.assertEqual(['Missing data, try !help advrem'], self.joe.message_history)
 
     def test_advrem_junk(self):
-        self.init_plugin("""
-            <configuration>
-                <settings name="settings">
-                    <set name="rate">45s</set>
-                </settings>
-                <ads>
-                    <ad>f00</ad>
-                    <ad>bar</ad>
-                    <ad>test</ad>
-                </ads>
-            </configuration>
-        """)
+        self.init_plugin(dedent(r"""
+            [settings]
+            rate: 45s
+            [ads]
+            f00
+            bar
+            test
+        """))
         self.assertEqual(['f00', 'bar', 'test'], self.p._msg.items)
         self.joe.clearMessageHistory()
         self.p.cmd_advrem(data='f00', client=self.joe)
@@ -374,18 +326,14 @@ class Test_commands(AdvTestCase):
         self.assertEqual(['Invalid data, use the !advlist command to list valid items numbers'], self.joe.message_history)
 
     def test_advrem_invalid_index(self):
-        self.init_plugin("""
-            <configuration>
-                <settings name="settings">
-                    <set name="rate">45s</set>
-                </settings>
-                <ads>
-                    <ad>f00</ad>
-                    <ad>bar</ad>
-                    <ad>test</ad>
-                </ads>
-            </configuration>
-        """)
+        self.init_plugin(dedent(r"""
+            [settings]
+            rate: 45s
+            [ads]
+            f00
+            bar
+            test
+        """))
         self.assertEqual(['f00', 'bar', 'test'], self.p._msg.items)
         self.joe.clearMessageHistory()
         self.p.cmd_advrem(data='-18', client=self.joe)
@@ -395,16 +343,12 @@ class Test_commands(AdvTestCase):
     #################### advadd ####################
 
     def test_advadd_nominal(self):
-        self.init_plugin("""
-            <configuration>
-                <settings name="settings">
-                    <set name="rate">45s</set>
-                </settings>
-                <ads>
-                    <ad>f00</ad>
-                </ads>
-            </configuration>
-        """)
+        self.init_plugin(dedent(r"""
+            [settings]
+            rate: 45s
+            [ads]
+            f00
+        """))
         self.assertEqual(['f00'], self.p._msg.items)
         self.joe.clearMessageHistory()
         self.p.cmd_advadd(data="bar", client=self.joe)
@@ -412,16 +356,12 @@ class Test_commands(AdvTestCase):
         self.assertEqual(['Adv: "bar" added'], self.joe.message_history)
 
     def test_advadd_no_arg(self):
-        self.init_plugin("""
-            <configuration>
-                <settings name="settings">
-                    <set name="rate">45s</set>
-                </settings>
-                <ads>
-                    <ad>f00</ad>
-                </ads>
-            </configuration>
-        """)
+        self.init_plugin(dedent(r"""
+            [settings]
+            rate: 45s
+            [ads]
+            f00
+        """))
         self.assertEqual(['f00'], self.p._msg.items)
         self.joe.clearMessageHistory()
         self.p.cmd_advadd(data=None, client=self.joe)
