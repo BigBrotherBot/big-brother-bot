BigBrotherBot/big-brother-bot

commit f01c3efb39dfd9d73444f726417f57de458d295f

[LOG] make log size configurable from b3 main configuration file

b3/parser.py
@@ -18,6 +18,7 @@
 #
 # CHANGELOG
 #
+# 2015/02/04 - 1.41.6 - Fenix           - optionally specify a log file size: 'logsize' option in 'b3' section of main cfg
 # 2015/02/02 - 1.41.5 - 82ndab.Bravo17  - Remove color codes at start of getWrap if not valid for game
 # 2015/01/29 - 1.41.4 - Fenix           - do not let plugins crash B3 by raising an exception within the constructor
 #                                       - fixed KeyError beiung raised in loadPlugins()
@@ -201,6 +202,7 @@ from b3.functions import getModule
 from b3.functions import vars2printf
 from b3.functions import main_is_frozen
 from b3.functions import splitDSN
+from b3.functions import getBytes
 from b3.functions import right_cut
 from textwrap import TextWrapper
 
@@ -365,7 +367,12 @@ class Parser(object):
         log2console = self.config.has_option('devmode', 'log2console') and \
             self.config.getboolean('devmode', 'log2console')
 
-        self.log = b3.output.getInstance(logfile, self.config.getint('b3', 'log_level'), log2console)
+        try:
+            logsize = self.config.get('b3', 'logsize')
+        except (TypeError, NoOptionError):
+            logsize = getBytes('10MB')
+
+        self.log = b3.output.getInstance(logfile, self.config.getint('b3', 'log_level'), logsize, log2console)
 
         # save screen output to self.screen
         self.screen = sys.stdout

b3/conf/b3.distribution.ini
@@ -22,9 +22,6 @@ log_level: 9
 # Name of the logfile the bot will generate
 logfile: b3.log
 # Comma separated list of plugins that will be loaded in 'disabled' status.
-# Example: if you want b3 to load the 'stats' and 'pingwatch' plugin but not to start them at b3 main run,
-# the following line should be >>> disabled_plugins: stats, pingwatch.
-# NOTE: if you specify here the name of a plugin which is not being loaded, B3 will simply discard it
 disabled_plugins:
 # The directory where additional plugins can be found
 external_plugins_dir: @b3/extplugins

b3/output.py
@@ -24,6 +24,7 @@
 # 20/04/2011 - 1.6.1 - Courgette - should get rid of UnicodeDecodeError
 # 20/04/2011 - 1.6.2 - Courgette - again getting rid of UnicodeDecodeError
 # 21/07/2014 - 1.7   - Fenix     - syntax cleanup
+# 04/02/2015 - 1.7.1 - Fenix     - getInstance() now accepts an optional 'logsize' parameter (amount of bytes of the log file)
 
 __author__  = 'ThorN'
 __version__ = '1.7'
@@ -147,11 +148,12 @@ logging.setLoggerClass(OutputHandler)
 
 __output = None
 
-def getInstance(logfile='b3.log', loglevel=21, log2console=False):
+def getInstance(logfile='b3.log', loglevel=21, logsize=10485760, log2console=False):
     """
     Return a Logger instance.
     :param logfile: The logfile name.
     :param loglevel: The logging level.
+    :param logsize: The size of the log file (in bytes)
     :log2console: Whether or not to extend logging to the console.
     """
     global __output
@@ -163,7 +165,7 @@ def getInstance(logfile='b3.log', loglevel=21, log2console=False):
 
         # add the log file handler
         file_formatter = logging.Formatter('%(asctime)s\t%(levelname)s\t%(message)r', '%y%m%d %H:%M:%S')
-        handler = handlers.RotatingFileHandler(logfile, maxBytes=10485760, backupCount=5, encoding="UTF-8")
+        handler = handlers.RotatingFileHandler(logfile, maxBytes=logsize, backupCount=5, encoding="UTF-8")
         handler.doRollover()
         handler.setFormatter(file_formatter)
 

commit aa3bc1cc46701f593840db313e51bf2b83e33c26

[PARSER] fixed typo introduced in f01c3efb39dfd9d73444f726417f57de458d295f

b3/parser.py
@@ -368,7 +368,7 @@ class Parser(object):
             self.config.getboolean('devmode', 'log2console')
 
         try:
-            logsize = self.config.get('b3', 'logsize')
+            logsize = getBytes(self.config.get('b3', 'logsize'))
         except (TypeError, NoOptionError):
             logsize = getBytes('10MB')
 