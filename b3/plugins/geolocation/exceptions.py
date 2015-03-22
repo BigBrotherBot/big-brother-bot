85a58dbab205e3eb9f56c654f9c2f96201e959ae
danielepantaleone <danielepantaleone@icloud.com>
2015-03-22 21:49:39 +0100

    [GEOLOCATION] added geolocation plugin

---
+++ b3/plugins/geolocation/exceptions.py
@@ -0,0 +1,27 @@
+#
+# Geolocation Plugin for BigBrotherBot(B3) (www.bigbrotherbot.net)
+# Copyright (C) 2015 Daniele Pantaleone <fenix@bigbrotherbot.net>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+class GeolocalizationError(Exception):
+    """
+    Raised when we are not able to geolocalize an IP address
+    """
+    def __init__(self, message):
+        Exception.__init__(self, message)
+
+    def __str__(self):
+        return repr(self.message)
