7fc1b447c228ac05b8a8f0ee51e06f392e02c35d
Daniele Pantaleone <danielepantaleone@me.com>
2015-04-15 11:37:14 +0200

    [TESTS] renamed fakeplugins_tmp to fakeplugins

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -28,7 +28,7 @@ from b3.config import CfgConfigParser
 from b3.plugin import Plugin
 from b3.events import Event
 from tests import B3TestCase
-from tests.fakeplugins_tmp import __file__ as external_plugins__file__
+from tests.fakeplugins import __file__ as external_plugins__file__
 
 external_plugins_dir = os.path.dirname(external_plugins__file__)
 testplugin_config_file = os.path.join(external_plugins_dir, "testplugin/conf/plugin_testplugin.ini")
