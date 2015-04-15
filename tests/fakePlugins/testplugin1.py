0f4f468aff533184a83bd0314a5c1e8640e7f2c7
Daniele Pantaleone <danielepantaleone@me.com>
2015-04-15 11:35:09 +0200

    [TESTS] temporary rename fakeplugins to fakeplugins_tmp
    Apparently git is case insensitive so when the previous commit changed `fakePlugins` to `fakeplugins`, the change was not forwarded to the remote repository since git detected no changes. Two commits are needed to solve this problem: http://stackoverflow.com/questions/3011625/git-mv-and-only-change-case-of-directory

--- tests/fakePlugins/testplugin1.py
+++
@@ -1,6 +0,0 @@
-from b3.plugin import Plugin
-
-
-class Testplugin1Plugin(Plugin):
-    requiresConfigFile = False
-    requiresParsers = []
--- tests/fakePlugins/testplugin2.py
+++
@@ -1,6 +0,0 @@
-from b3.plugin import Plugin
-
-
-class Testplugin2Plugin(Plugin):
-    requiresConfigFile = False
-    requiresParsers = ["Dummy"]
