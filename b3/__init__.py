95a244cf2ac1e7706e1b8f1185f7d45f9553c9cd
Thomas LÉVEIL <thomasleveil@gmail.com>
2015-06-23 23:57:28 +0200

    bump 1.10.1

--- b3/__init__.py
+++ b3/__init__.py
@@ -53,7 +53,7 @@ from ConfigParser import NoOptionError
 from ConfigParser import NoSectionError
 
 __author__ = 'ThorN'
-__version__ = '1.10.0'
+__version__ = '1.10.1'
 
 modulePath = pkg_handler.resource_directory(__name__)
 
