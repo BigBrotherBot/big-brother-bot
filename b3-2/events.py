80c8c7fd032a35ef097988e6c85a3152d9254fdc
Daniele Pantaleone <dpantaleone@domotz.com>
2018-03-18 12:52:34 +0100

    Basic event manager implementation

--- b3-2/events.py
+++ b3-2/events.py
@@ -23,13 +23,21 @@
 # ################################################################### #
 
 
-from clients import Client
-from enum import Enum, unique
-from time import time
+import aenum
+import configparser
+import queue
+import threading
+import time
+import traceback
+import typing
+
+from .clients import Client
+from .output import LoggerMixin
+from .plugin import Plugin
 
 
-@unique
-class EventType(Enum):
+@aenum.unique
+class EventType(aenum.Enum):
     """B3 parser indipendent event type definitions"""
     EXIT = 'Program exit'
     CLIENT_AUTH = 'Client authentication'
@@ -59,15 +67,130 @@ class EventType(Enum):
     PLUGIN_DISABLED = 'Plugin disabled'
     PLUGIN_ENABLED = 'Plugin enabled'
 
+    @classmethod
+    def extend(cls, name:str, description:str):
+        """Dynamically create a new event type"""
+        aenum.extend_enum(cls, name, description)
+
+
 class Event(object):
+    """B3 event representation"""
 
     def __init__(self, type:EventType, data:dict, client:Client=None, target:Client=None):
-        """B3 event representation"""
-        self.time : float = time()
-        self.type : EventType = type
-        self.data : dict = data
-        self.client : Client = client
-        self.target : Client = target
+        super(Event, self).__init__()
+        self.type = type
+        self.data = data
+        self.client = client
+        self.target = target
+        self.expire = 0
+        self.time = 0
 
     def __str__(self):
         return 'Event<%s>(%r, %s, %s)' % (self.type.value, self.data, self.client, self.target)
+
+
+class EventManager(LoggerMixin, object):
+    """B3 event manager implementation"""
+
+    def __init__(self, config:configparser.ConfigParser):
+        super(EventManager, self).__init__()
+        self._running = True
+        self._event_queue_size = config.getint('b3', 'event_queue_size', fallback=50)
+        self._event_queue_expire_time = config.getint('b3', 'event_queue_expire_time', fallback=10)
+        self._handlers:typing.Dict[EventType, typing.List[Plugin]] = {}
+        self._lock = threading.Lock()
+        self.debug("Creating the event queue with size %s", self._event_queue_size)
+        self.queue = queue.Queue(self._event_queue_size)
+
+    # #########################################################################
+    # EVENT MANAGEMENT
+    # #########################################################################
+
+    def post(self, event:Event, expire:int=None) -> bool:
+        """Enqueue the given event for later processing"""
+        if event.type in self._handlers:
+            if expire is None:
+                expire = self._event_queue_expire_time
+            event.time = time.time()
+            event.expire = event.time + expire
+            try:
+                time.sleep(0.001)  # wait a bit so event doesnt get jumbled
+                self.verbose('Queuing event %s', event)
+                self.queue.put(event, True, 2)
+                return True
+            except queue.Full:
+                self.error('Event queue is full (%d)', self.queue.qsize())
+                return False
+        return False
+
+    def subscribe(self, handler:Plugin, event:EventType):
+        """Register an handler for the given event type"""
+        self.debug('%s: register event <%s>', handler.__class__.__name__, event.value)
+        if event not in self._handlers:
+            self._handlers[event] = []
+        if handler not in self._handlers[event]:
+            self._handlers[event].append(handler)
+
+    def unsubscribe(self, handler:Plugin, event:EventType=None):
+        """Unregister an event handler"""
+        for e in self._handlers:
+            if event is None or e is event:
+                if handler in self._handlers[e]:
+                    self.debug('%s: unregister event <%s>', handler.__class__.__name__, e.value)
+                    self._handlers[e].remove(handler)
+
+    # #########################################################################
+    # MAIN
+    # #########################################################################
+
+    def running(self) -> bool:
+        """Returns True if the event manager is running, False otherwise"""
+        with self._lock:
+            return self._running
+
+    def shutdown(self):
+        """Shutdown the event manager preving further event processing"""
+        with self._lock:
+            self._running = False
+
+    def start(self):
+        """Main event loop"""
+        self.debug('Event manager started processing events')
+        while self.running():
+            event = self.queue.get(True)
+            if event.type == EventType.EXIT:
+                self.shutdown()
+            if time.time() >= event.expire: # Events can only sit in the queue until expire time.
+                self.error('Event <%s> sat in queue too long: %s %fms', event.value, (time.time() - event.expire) * 1000)
+            else:
+                # We need to check again for the event manager to be alive because
+                # queue.get blocks until an event is available for processing and another
+                # thread may have forced the event manager to shutdown.
+                if self.running():
+                    nomore = False
+                    for handler in self._handlers[event.type]:
+                        if not handler.isEnabled():
+                            continue
+                        if nomore:
+                            break
+                        self.verbose('%s: processing event <%s>', handler.__class__.__name__, event.value)
+                        start = time.process_time()
+                        try:
+                            handler.onEvent(event)
+                        except VetoEvent:
+                            self.verbose('Event <%s> vetoed by %s', event.value, handler.__class__.__name__)
+                            nomore = True
+                        except Exception as e:
+                            self.error('%s: could not process event <%s> : %s',
+                                       handler.__class__.__name__, event.value,
+                                       ''.join(traceback.format_exception(type(e), e, e.__traceback__)))
+                        else:
+                            elapsed = (time.process_time() - start) * 1000
+                            self.verbose('Event <%s> processed by %s in %fms',
+                                         event.value, handler.__class__.__name__, elapsed)
+        self.debug('Event manager stopped processing events')
+
+
+class VetoEvent(Exception):
+    """Raised to cancel event processing"""
+    pass
