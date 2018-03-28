961605497a93e0b4f9cf1011a2e54a83ca73f9f3
Daniele Pantaleone <dpantaleone@domotz.com>
2018-03-28 20:46:07 +0200

    Added event router and allow to dynamically create event types

--- b3/events.py
+++ b3/events.py
@@ -2,8 +2,8 @@
 
 # ################################################################### #
 #                                                                     #
-#  BigBrotherBot(B3) (www.bigbrotherbot.net)                          #
-#  Copyright (C) 2005 Michael "ThorN" Thornton                        #
+#  BigBrotherBot (B3) (www.bigbrotherbot.net)                         #
+#  Copyright (C) 2018 Daniele Pantaleone <danielepantaleone@me.com>   #
 #                                                                     #
 #  This program is free software; you can redistribute it and/or      #
 #  modify it under the terms of the GNU General Public License        #
@@ -22,224 +22,242 @@
 #                                                                     #
 # ################################################################### #
 
-__author__ = 'ThorN, xlr8or, Courgette'
-__version__ = '1.8.2'
 
+import aenum
+import configparser
+import queue
 import re
+import threading
 import time
+import traceback
+import typing
 
-from b3.functions import meanstdv
-from b3.decorators import Memoize
-from b3.output import VERBOSE
-from collections import deque
-from logging import DEBUG
+from .clients import Client
+from .output import LoggerMixin
+from .plugin import Plugin
 
 
-class Events:
+@aenum.unique
+class EventType(aenum.Enum):
+    """B3 parser indipendent event type definitions"""
+    EXIT = 'Program exit'
+    CLIENT_AUTH = 'Client authentication'
+    CLIENT_BAN = 'Client ban'
+    CLIENT_CONNECT = 'Client connect'
+    CLIENT_DISCONNECT = 'Client disconnect'
+    CLIENT_DAMAGE = 'Client damage'
+    CLIENT_DAMAGE_TEAM = 'Client team damage'
+    CLIENT_DAMAGE_SELF = 'Client self damage'
+    CLIENT_KICK = 'Client kick'
+    CLIENT_KILL = 'Client kill'
+    CLIENT_KILL_TEAM = 'Client team kill'
+    CLIENT_KILL_SELF = 'Client suicide'
+    CLIENT_NAME_CHANGE = 'Client name change'
+    CLIENT_NOTICE = 'Client notice'
+    CLIENT_SAY = 'Client say'
+    CLIENT_SAY_TEAM = 'Client say team'
+    CLIENT_SAY_PRIVATE = 'Client private message'
+    CLIENT_TEAM_CHANGE = 'Client team change'
+    CLIENT_TEAM_JOIN = 'Client team join'
+    CLIENT_UNBAN = 'Client unban'
+    CLIENT_WARNING = 'Client warning'
+    GAME_EXIT = 'Game exit'
+    GAME_MAP_CHANGE = 'Game map change'
+    GAME_ROUND_START = 'Game round start'
+    GAME_ROUND_END = 'Game round end'
+    GAME_WARMUP = 'Game warmup'
+    PLUGIN_DISABLED = 'Plugin disabled'
+    PLUGIN_ENABLED = 'Plugin enabled'
 
-    def __init__(self):
-        """
-        Object constructor.
-        """
-        self._events = {}
-        self._eventNames = {}
-
-        self.loadEvents((
-            ('EVT_EXIT', 'Program Exit'),
-            ('EVT_STOP', 'Stop Process'),
-            ('EVT_UNKNOWN', 'Unknown Event'),
-            ('EVT_CUSTOM', 'Custom Event'),
-            ('EVT_PLUGIN_ENABLED', 'Plugin Enabled'),
-            ('EVT_PLUGIN_DISABLED', 'Plugin Disabled'),
-            ('EVT_PLUGIN_LOADED', 'Plugin Loaded'),
-            ('EVT_PLUGIN_UNLOADED', 'Plugin Unloaded'),
-            ('EVT_CLIENT_SAY', 'Say'),
-            ('EVT_CLIENT_TEAM_SAY', 'Team Say'),
-            ('EVT_CLIENT_SQUAD_SAY', 'Squad Say'),
-            ('EVT_CLIENT_PRIVATE_SAY', 'Private Message'),
-            ('EVT_CLIENT_CONNECT', 'Client Connect'),
-            ('EVT_CLIENT_AUTH', 'Client Authenticated'),
-            ('EVT_CLIENT_DISCONNECT', 'Client Disconnect'),
-            ('EVT_CLIENT_UPDATE', 'Client Update'),
-            ('EVT_CLIENT_KILL', 'Client Kill'),
-            ('EVT_CLIENT_GIB', 'Client Gib'),
-            ('EVT_CLIENT_GIB_TEAM', 'Client Gib Team'),
-            ('EVT_CLIENT_GIB_SELF', 'Client Gib Self'),
-            ('EVT_CLIENT_SUICIDE', 'Client Suicide'),
-            ('EVT_CLIENT_KILL_TEAM', 'Client Team Kill'),
-            ('EVT_CLIENT_DAMAGE', 'Client Damage'),
-            ('EVT_CLIENT_DAMAGE_SELF', 'Client Damage Self'),
-            ('EVT_CLIENT_DAMAGE_TEAM', 'Client Team Damage'),
-            ('EVT_CLIENT_JOIN', 'Client Join Team'),
-            ('EVT_CLIENT_NAME_CHANGE', 'Client Name Change'),
-            ('EVT_CLIENT_TEAM_CHANGE', 'Client Team Change'),     # provides only the new team
-            ('EVT_CLIENT_TEAM_CHANGE2', 'Client Team Change 2'),  # provides the previous and new team
-            ('EVT_CLIENT_ITEM_PICKUP', 'Client Item Pickup'),
-            ('EVT_CLIENT_ACTION', 'Client Action'),
-            ('EVT_CLIENT_KICK', 'Client Kicked'),
-            ('EVT_CLIENT_BAN', 'Client Banned'),
-            ('EVT_CLIENT_BAN_TEMP', 'Client Temp Banned'),
-            ('EVT_CLIENT_UNBAN', 'Client Unbanned'),
-            ('EVT_CLIENT_WARN', 'Client Warned'),
-            ('EVT_CLIENT_NOTICE', 'Client given a notice'),
-            ('EVT_GAME_ROUND_START', 'Game Round Start'),
-            ('EVT_GAME_ROUND_END', 'Game Round End'),
-            ('EVT_GAME_WARMUP', 'Game Warmup'),
-            ('EVT_GAME_EXIT', 'Game Exit'),
-            ('EVT_GAME_MAP_CHANGE', 'map changed'),
-        ))        
-
-    def createEvent(self, key, name=None):
-        """
-        Create an event.
-        :param key: The event key
-        :param name: An optional name to associate to the event
-        """
-        g = globals()
-
-        try:
-            _id = self._events[key] = g[key]
-        except KeyError:
-            _id = self._events[key] = len(self._events) + 1
-
-        if name:
-            self._eventNames[_id] = name
-        else:
-            self._eventNames[_id] = 'Unnamed (%s)' % key
-
-        g[key] = _id
-        return _id
-
-    def getId(self, key):
-        """
-        Return an event ID given its key.
-        :param key: The event key
-        """
-        if re.match('^[0-9]+$', str(key)):
-            return int(key)
-        else:
-            try:
-                return self._events[key]
-            except KeyError:
-                return None
-
-    @Memoize
-    def getKey(self, event_id):
-        """
-        Get the key of a given event ID.
-        :param event_id: The event ID
-        """
-        matching_keys = [k for k, v in self._events.iteritems() if v == event_id]
-        if not len(matching_keys):
-            raise KeyError('could not find any B3 event with ID %s' % event_id)
-        assert len(matching_keys) == 1, 'expecting only one event key per event ID: %r' % matching_keys
-        return matching_keys[0]
-
-    def getName(self, key):
-        """
-        Return an event name given its key.
-        :param key: The event key
-        """
-        try:
-            return self._eventNames[self.getId(key)]
-        except KeyError:
-            return 'Unknown (%s)' % key
-
-    def loadEvents(self, events):
-        """
-        Load default events.
-        :param events: A collection of Event tuples
-        """
-        for k, n in events:
-            self.createEvent(k, n)
-
-    def _get_events(self):
-        """
-        Return the Event dict.
-        """
-        return self._events
-
-    events = property(_get_events)
+    @classmethod
+    def extend(cls, name:str, description:str):
+        """Dynamically create a new event type"""
+        aenum.extend_enum(cls, name, description)
 
 
 class Event(object):
+    """B3 event representation"""
 
-    def __init__(self, type, data, client=None, target=None):
-        """
-        Object constructor.
-        :param type: The event ID
-        :param data: Event data
-        :param client: The client source of this event
-        :param target: The target of this event
-        """
-        self.time = int(time.time())
+    def __init__(self, type:EventType, data:dict, client:Client=None, target:Client=None):
+        super(Event, self).__init__()
         self.type = type
         self.data = data
         self.client = client
         self.target = target
+        self.expire = 0
+        self.time = 0
 
     def __str__(self):
-        return "Event<%s>(%r, %s, %s)" % (eventManager.getKey(self.type), self.data, self.client, self.target)
+        return 'Event<%s>(%r, %s, %s)' % (self.type.value, self.data, self.client, self.target)
 
 
-class EventsStats(object):
+class EventManager(LoggerMixin, object):
+    """B3 event manager implementation"""
 
-    def __init__(self, console, max_samples=100):
-        """
-        Object constructor.
-        :param console: The console class instance
-        :param max_samples: The size of the event queue
-        """
-        self.console = console
-        self._max_samples = max_samples
-        self._handling_timers = {}
-        self._queue_wait = deque(maxlen=max_samples)
-        
-    def add_event_handled(self, plugin_name, event_name, milliseconds_elapsed):
-        """
-        Add an event to the dict of handled ones.
-        :param plugin_name: The name of the plugin which handled the event
-        :param event_name: The event name
-        :param milliseconds_elapsed: The amount of milliseconds necessary to handle the event
-        """
-        if plugin_name not in self._handling_timers:
-            self._handling_timers[plugin_name] = {}
-        if event_name not in self._handling_timers[plugin_name]:
-            self._handling_timers[plugin_name][event_name] = deque(maxlen=self._max_samples)
-        self._handling_timers[plugin_name][event_name].append(milliseconds_elapsed)
-        self.console.verbose2("%s event handled by %s in %0.3f ms", event_name, plugin_name, milliseconds_elapsed)
+    def __init__(self, config:configparser.ConfigParser):
+        super(EventManager, self).__init__()
+        self._running = True
+        self._event_queue_size = config.getint('b3', 'event_queue_size', fallback=50)
+        self._event_queue_expire_time = config.getint('b3', 'event_queue_expire_time', fallback=10)
+        self._handlers:typing.Dict[EventType, typing.List[Plugin]] = {}
+        self._lock = threading.RLock()
+        self.debug("Creating the event queue with size %s", self._event_queue_size)
+        self.queue = queue.Queue(self._event_queue_size)
 
-    def add_event_wait(self, milliseconds_wait):
-        """
-        Add delay to the event processing.
-        :param milliseconds_wait: The amount of milliseconds to wait
-        """
-        self._queue_wait.append(milliseconds_wait)
-        
-    def dumpStats(self):
-        """
-        Print event stats in the log file.
-        """
-        if self.console.log.isEnabledFor(VERBOSE):
-            for plugin_name, plugin_timers in self._handling_timers.iteritems():
-                for event_name, event_timers in plugin_timers.iteritems():
-                    mean, stdv = meanstdv(event_timers)
-                    if len(event_timers):
-                        self.console.verbose("%s %s : (ms) min(%0.1f), max(%0.1f), mean(%0.1f), "
-                                             "stddev(%0.1f)", plugin_name, event_name, min(event_timers),
-                                             max(event_timers), mean, stdv)
+    # #########################################################################
+    # EVENT MANAGEMENT
+    # #########################################################################
 
-        if self.console.log.isEnabledFor(DEBUG):
-            mean, stdv = meanstdv(self._queue_wait)
-            if len(self._queue_wait):
-                self.console.debug("Events waiting in queue stats : (ms) min(%0.1f), max(%0.1f), mean(%0.1f), "
-                                   "stddev(%0.1f)", min(self._queue_wait), max(self._queue_wait), mean, stdv)
-    
+    def create(self, name:str, description:str):
+        """Dynamically create a new event type"""
+        self.debug('Creating event %s (%s)', name, description)
+        EventType.extend(name, description)
+
+    def post(self, event:Event, expire:int=None) -> bool:
+        """Enqueue the given event for later processing"""
+        if event.type in self._handlers:
+            if expire is None:
+                expire = self._event_queue_expire_time
+            event.time = time.time()
+            event.expire = event.time + expire
+            try:
+                time.sleep(0.001)  # wait a bit so event doesnt get jumbled
+                self.verbose('Queuing event %s', event)
+                self.queue.put(event, True, 2)
+                return True
+            except queue.Full:
+                self.error('Event queue is full (%d)', self.queue.qsize())
+                return False
+        return False
+
+    def subscribe(self, handler:Plugin, event:EventType):
+        """Register an handler for the given event type"""
+        self.debug('%s: register event <%s>', handler.__class__.__name__, event.value)
+        if event not in self._handlers:
+            self._handlers[event] = []
+        if handler not in self._handlers[event]:
+            self._handlers[event].append(handler)
+
+    def unsubscribe(self, handler:Plugin, event:EventType=None):
+        """Unregister an event handler"""
+        for e in self._handlers:
+            if event is None or e is event:
+                if handler in self._handlers[e]:
+                    self.debug('%s: unregister event <%s>', handler.__class__.__name__, e.value)
+                    self._handlers[e].remove(handler)
+
+    # #########################################################################
+    # MAIN
+    # #########################################################################
+
+    def running(self) -> bool:
+        """Returns True if the event manager is running, False otherwise"""
+        with self._lock:
+            return self._running
+
+    def shutdown(self):
+        """Shutdown the event manager preving further event processing"""
+        with self._lock:
+            self._running = False
+
+    def start(self):
+        """Main event loop"""
+        self.debug('Event manager started processing events')
+        while self.running():
+            event = self.queue.get(True)
+            if event.type == EventType.EXIT:
+                self.shutdown()
+            if time.time() >= event.expire: # Events can only sit in the queue until expire time.
+                self.error('Event <%s> sat in queue too long: %s %fms', event.value, (time.time() - event.expire) * 1000)
+            else:
+                # We need to check again for the event manager to be alive because
+                # queue.get blocks until an event is available for processing and another
+                # thread may have forced the event manager to shutdown.
+                if self.running():
+                    nomore = False
+                    for handler in self._handlers[event.type]:
+                        if not handler.isEnabled():
+                            continue
+                        if nomore:
+                            break
+                        self.verbose('%s: processing event <%s>', handler.__class__.__name__, event.value)
+                        start = time.process_time()
+                        try:
+                            handler.onEvent(event)
+                        except VetoEvent:
+                            self.verbose('Event <%s> vetoed by %s', event.value, handler.__class__.__name__)
+                            nomore = True
+                        except Exception as e:
+                            self.error('%s: could not process event <%s> : %s',
+                                       handler.__class__.__name__, event.value,
+                                       ''.join(traceback.format_exception(type(e), e, e.__traceback__)))
+                        else:
+                            elapsed = (time.process_time() - start) * 1000
+                            self.verbose('Event <%s> processed by %s in %fms',
+                                         event.value, handler.__class__.__name__, elapsed)
+        self.debug('Event manager stopped processing events')
+
+
+class EventRouter(object):
+    """
+    This class helps defining handlers functions to be called when a game event is received.
+    This class provides:
+      - A decorator for associating a regular expression to a function: @router.gameEvent(<regular expression>, ...)
+      - A method handler(<text>) that return a tuple <func, dict> where:
+          * func is the first handler function defined with the 'event' decorator that matches the given *text*
+          * dict is a dictionary of all matched groups from the regular expression that was associated to *func*
+            with the decorator.
+
+    USAGE :
+    -------
+    This tool is meant to be used by B3 game parsers which need to parse game event (in the
+    form of a string) and call a function that takes action for this type of game event.
+
+    >>> from b3.events import EventRouter
+    >>>
+    >>> router = EventRouter()
+    >>>
+    >>> def parse(self, data:str):
+    >>>     if data:
+    >>>         func, params = router.handler(data)
+    >>>         if func:
+    >>>             func(self, **params)
+
+    Let say you need to print "Robin joined team BLUE" when parsing a game event which is
+    "join: Robin, BLUE" then you would have the following method in your B3 game parser:
+
+    >>> @router.gameEvent("^join: (?P<name>.+), (?P<team>.+)$")
+    >>> def onConnect(self, name:str, team:str):
+    >>>     print("%s joined team %s", name, team)
+
+    Note that the handler function must have parameters that matches the regular expression groups.
+    The @router.gameEvent decorator accepts multiple parameters if you need to have one handling
+    function for multiple kind of game events. Note that those regular expressions should all define
+    the same groups.
+    """
+    def __init__(self):
+        self._compiled_retype = type(re.compile(''))
+        self._mappings = []
+
+    def gameEvent(self, *params):
+        def wrapper(func):
+            for param in params:
+                if isinstance(param, self._compiled_retype):
+                    self._mappings.append((param, func))
+                elif isinstance(param, str):
+                    self._mappings.append((re.compile(param), func))
+            return func
+        return wrapper
+
+    def handler(self, gameEvent:str):
+        """Returns the (func, dict) matching the given game event"""
+        for r, func in self._mappings:
+            m = r.match(gameEvent)
+            if m:
+                return func, m.groupdict()
+        return None, {}
+
 
 class VetoEvent(Exception):
-    """
-    Raised to cancel event processing.
-    """
+    """Raised to cancel event processing"""
     pass
-
-eventManager = Events()
