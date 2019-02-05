cc4fed441f2b21defef90dbf4ffda242993f841c
Daniele Pantaleone <dpantaleone@domotz.com>
2019-02-05 18:44:02 +0100

    Added plugin manager and implemented plugin startup sequence

--- b3/functions.py
+++ b3/functions.py
@@ -26,7 +26,7 @@
 import re
 
 
-def getBytes(size:str) -> int:
+def str_to_bytes(size:str) -> int:
     """Returns the amount of bytes identified by the given string format"""
     size = size.upper()
     r = re.compile(r'''^(?P<size>\d+)\s*(?P<multiplier>KB|MB|GB|TB|K|M|G|T?)$''')
@@ -51,3 +51,29 @@ def rchop(string:str, ending:str) -> str:
     if string.endswith(ending):
         return string[:-len(ending)]
     return string
+
+
+def topological_sort(source:list):
+    """
+    Perform topological sort on elements using generators.
+    http://stackoverflow.com/questions/11557241/python-sorting-a-dependency-list
+    :param source: list of (name, set(names of dependancies)) pairs
+    """
+    pending = [(name, set(deps)) for name, deps in source] # copy deps so we can modify set in-place
+    emitted = []
+    while pending:
+        next_pending = []
+        next_emitted = []
+        for entry in pending:
+            name, deps = entry
+            deps.difference_update(emitted) # remove deps we emitted last pass
+            if deps: # still has deps? recheck during next pass
+                next_pending.append(entry)
+            else: # no more deps? time to emit
+                yield name
+                emitted.append(name) # <-- not required, but helps preserve original ordering
+                next_emitted.append(name) # remember what we emitted for difference_update() in next pass
+        if not next_emitted: # all entries have unmet deps, one of two things is wrong...
+            raise RuntimeError("cyclic or missing dependancy detected: %r" % (next_pending,))
+        pending = next_pending
+        emitted = next_emitted
--- b3/plugin.py
+++ b3/plugin.py
@@ -24,12 +24,21 @@
 
 
 import abc
+
 import b3
+import collections
 import configparser
+import re
 import typing
+import types
+
+from distutils.version import StrictVersion
+from importlib.machinery import SourceFileLoader
 
 from .events import Event, EventType
-from .functions import rchop
+from .files import FileManager
+from .functions import rchop, topological_sort
+from .output import LoggerMixin
 from .parser import Parser
 
 
@@ -38,14 +47,14 @@ class Plugin(object):
 
     __metaclass__ = abc.ABCMeta
 
-    # #########################################################################
+    # -----------------------------------------------------
     # PLUGIN REQUIREMENTS
-    # #########################################################################
+    # -----------------------------------------------------
 
-    required_parsers: typing.Set['Parser'] = {}
+    required_parsers: typing.Set[str] = {}
     """Set of parsers supported by the plugin (if empty B3 assumes the plugin non-parser dependent)"""
 
-    required_plugins: typing.Set['Plugin'] = {}
+    required_plugins: typing.Set[str] = {}
     """Set of plugins required to run this very plugin (if one of these is missing, the plugin won't be loaded)"""
 
     required_version: str = b3.__version__
@@ -57,60 +66,58 @@ class Plugin(object):
         self.console = console
         self._enabled = True
 
-    # #########################################################################
+    # -----------------------------------------------------
     # PROPERTIES
-    # #########################################################################
+    # -----------------------------------------------------
 
     @property
     def name(self):
         """Returns the plugin name"""
         return rchop(self.__class__.__name__.lower(), 'plugin')
 
-    # #########################################################################
+    # -----------------------------------------------------
     # MISC
-    # #########################################################################
+    # -----------------------------------------------------
 
     def enable(self):
         """Enable the plugin"""
         self._enabled = True
-        self.onEnable()
+        self.onEnable() # this very plugin should be notified instantly while other plugins wait for the event
         self.console.events.post(Event(type=EventType.PLUGIN_ENABLED, data={'name':self.name}))
 
     def disable(self):
         """Disable the plugin"""
         self._enabled = False
-        self.onDisable()
-        self.console.events.post(Event(type=EventType.PLUGIN_DISABLED, data={'name': self.name}))
+        self.onDisable() # this very plugin should be notified instantly while other plugins wait for the event
+        self.console.events.post(Event(type=EventType.PLUGIN_DISABLED, data={'name':self.name}))
 
     def isEnabled(self) -> bool:
         """Returns True if the plugin is enabled, False otherwise"""
         return self._enabled
 
-    # #########################################################################
+    # -----------------------------------------------------
     # HOOKS
-    # #########################################################################
+    # -----------------------------------------------------
 
     def onDisable(self):
         """Executed when the plugin is disabled"""
+        pass
 
     def onEnable(self):
         """Executed when the plugin is enabled"""
+        pass
 
     def onEvent(self, event:Event):
         """Executed when an event is received by the plugin"""
         pass
 
-    def onLoadConfig(self, config:configparser.ConfigParser):
-        """Executed by the parser to allow plugins to load a configuration file"""
-        pass
-
     def onStartup(self):
-        """Executed by the parser the allow plugins to perform startup operations"""
+        """Executed by the parser tp allow plugins to perform startup operations"""
         pass
 
-    # #########################################################################
+    # -----------------------------------------------------
     # LOGGING
-    # #########################################################################
+    # -----------------------------------------------------
 
     def debug(self, msg:str, *args, **kwargs):
         """Log a DEBUG message"""
@@ -131,3 +138,128 @@ class Plugin(object):
     def warning(self, msg:str, *args, **kwargs):
         """Log a WARNING message"""
         self.console.warning('%s: %s' % (self.__class__.__name__, msg), *args, **kwargs)
+
+
+class PluginManager(LoggerMixin, object):
+    """B3 plugin manager implementation"""
+
+    def __init__(self, console:Parser, config:configparser.ConfigParser):
+        super(PluginManager, self).__init__()
+        self._plugins = collections.OrderedDict()
+        self.fs = FileManager()
+        self.console = console
+        self.config = config
+
+    def init_plugins(self):
+        """Load all the plugins and load their configuration file"""
+        plugins_found:typing.Dict[str, tuple] = {} # unsorted dict of available plugins
+        plugins_ok:typing.Dict[str, tuple] = {} # unsorted dict of plugins that meets dependencies
+        plugins_sorted:typing.List[str] = [] # list of plugin names sorted according to their requirements
+
+        def find_plugin_module(name:str) -> typing.Tuple[types.ModuleType, str]:
+            root = self.fs.join('@plugins', name, name)
+            for extension in ('.pyc', '.pyo', '.py'):
+                path = '{0}{1}'.format(root, extension)
+                if self.fs.isFile(path):
+                    self.verbose('Found plugin %s module at %s', name, path)
+                    break
+            else:
+                raise IOError('no module named {0} at {1}.py[c|o]'.format(name, root))
+            module = SourceFileLoader(name, path).load_module()
+            return module, path
+
+        def find_plugin_class(module:types.ModuleType, path:str, name:str) -> typing.Type['Plugin']:
+            imp = '{0}Plugin'.format(''.join(x.title() for x in list(filter(None, re.split("[_\-]+", name)))))
+            if not hasattr(module, imp):
+                raise AttributeError('no class named {0} found in module {1}'.format(imp, path))
+            return getattr(module, imp)
+
+        def find_plugin_config(name:str):
+            path = self.fs.expand(self.config.get('plugins', plugin_name))
+            if not self.fs.isFile(path):
+                raise IOError('missing configuration file {0} for plugin {1}'.format(path, name))
+            self.verbose('Found plugin %s configuration at %s', name, path)
+            return path
+
+        # --- SEARCHING PLUGINS ---------------------------
+
+        if self.config.has_section('plugins'):
+
+            options = self.config.options('plugins')
+            self.debug('Searching %d plugins: %s', len(options), ', '.join(options))
+            for plugin_name in options:
+                try:
+                    plugin_module, plugin_path = find_plugin_module(plugin_name)
+                    plugin_imp = find_plugin_class(plugin_module, plugin_path, plugin_name)
+                    plugin_config = find_plugin_config(plugin_name)
+                except Exception:
+                    self.exception('Could not load plugin %s', plugin_name)
+                else:
+                    plugins_found[plugin_name] = (plugin_module, plugin_imp, plugin_config)
+
+            self.debug('Found %d plugins: %s', len(plugins_found), ', '.join(x for x in plugins_found))
+
+        # --- CHECK REQUIREMENTS --------------------------
+
+        if 'admin' not in plugins_found:
+            self.critical('Missing admin plugin: cannot continue without admin plugin!')
+
+        for plugin_name in plugins_found:
+
+            try:
+                plugin_imp = plugins_found[plugin_name][1]
+                if plugin_imp.required_version and StrictVersion(plugin_imp.required_version) > StrictVersion(b3.__version__):
+                    raise RuntimeError('minimum B3 required version is {0}'.format(plugin_imp.required_version))
+                if plugin_imp.required_parsers and self.console.name not in plugin_imp.required_parsers:
+                    raise RuntimeError('supported games are <{0}>'.format(','.join(plugin_imp.required_parsers)))
+                if plugin_imp.required_plugins:
+                    for plugin in plugin_imp.required_plugins:
+                        if plugin not in plugins_found:
+                            raise RuntimeError('additional required plugins are <{0}>'.format(','.join(plugin_imp.required_plugins)))
+            except Exception:
+                self.exception('Missing requirement in %s plugin', plugin_name)
+            else:
+                plugins_ok[plugin_name] = plugins_found[plugin_name]
+
+        # --- SORT ACCORDING TO THEIR REQUIREMENTS --------
+
+        for plugin_name in [y for y in \
+            topological_sort([(plugin_name, set(plugins_ok[plugin_name][1].required_plugins)) \
+                for plugin_name in plugins_ok])]:
+            plugins_sorted.append(plugin_name)
+        plugins_sorted.remove('admin')
+        plugins_sorted.insert(0, 'admin')
+
+        # --- CREATE INSTANCES ----------------------------
+
+        i = 1
+        for plugin_name in plugins_sorted:
+            try:
+                plugin_module = plugins_ok[plugin_name][0]
+                plugin_imp = plugins_ok[plugin_name][1]
+                plugin_config = plugins_ok[plugin_name][2]
+                self.debug('Loading plugin #%s : %s [%s]', i, plugin_name, plugin_config)
+                config = configparser.ConfigParser()
+                config.read(plugin_config)
+                plugin = plugin_imp(console=self.console, config=config)
+            except Exception:
+                self.exception('Could not create %s plugin instance', plugin_name)
+            else:
+                i += 1
+                self._plugins[plugin_name] = plugin
+                version = getattr(plugin_module, '__version__', '<unknown>')
+                author = getattr(plugin_module, '__author__', '<unknown>')
+                self.debug('Loaded plugin %s version %s by %s)', plugin_name, version, author)
+
+    def start_plugins(self):
+        """Start all the loaded plugins"""
+        i = 1
+        for plugin_name in self._plugins:
+            try:
+                self.debug('Starting plugin #%s: %s', i, plugin_name)
+                plugin = self._plugins[plugin_name]
+                plugin.startup()
+            except Exception:
+                self.exception('Could not start %s plugin', plugin_name)
+            else:
+                i += 1
